70aa7574ea00cfad83464be0a97465117ea10721
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-09-19 09:14:28 +0200

    Checkin time.

---
+++ reportCriticalPaths.tcl
@@ -0,0 +1,39 @@
+#------------------------------------------------------------------------
+# reportCriticalPaths
+#------------------------------------------------------------------------
+# This function generates a CSV file that provides a summary of the first
+# 50 violations for both Setup and Hold analysis. So a maximum number of
+# 100 paths are reported.
+#------------------------------------------------------------------------
+proc reportCriticalPaths { fileName } {
+    # Open the specified output file in write mode
+    set FH [open $fileName w]
+    # Write the current date and CSV format to a file header
+    puts $FH "#\n# File created on [clock format [clock seconds]]\n#\n"
+    puts $FH "Startpoint,Endpoint,DelayType,Slack,#Levels,#LUTs"
+    # Iterate through both Min and Max delay types
+    foreach delayType {max min} {
+        # Collect details from the 50 worst timing paths for the current analysis
+        # (max = setup/recovery, min = hold/removal)
+        # The $path variable contains a Timing Path object.
+        foreach path [get_timing_paths -delay_type $delayType -max_paths 50 -nworst 1] {
+            # Get the LUT cells of the timing paths
+            set luts [get_cells -filter {REF_NAME =~ LUT*} -of_object $path]
+            # Get the startpoint of the Timing Path object
+            set startpoint [get_property STARTPOINT_PIN $path]
+            # Get the endpoint of the Timing Path object
+            set endpoint [get_property ENDPOINT_PIN $path]
+            # Get the slack on the Timing Path object
+            set slack [get_property SLACK $path]
+            # Get the number of logic levels between startpoint and endpoint
+            set levels [get_property LOGIC_LEVELS $path]
+            # Save the collected path details to the CSV file
+            puts $FH "$startpoint,$endpoint,$delayType,$slack,$levels,[llength $luts]"
+        }
+    }
+    # Close the output file
+    close $FH
+    puts "CSV file $fileName has been created.\n"
+    return 0
+}; # End PROC
+
--- xilinx.mk
+++ xilinx.mk
@@ -1,7 +1,9 @@
+# This Makefile is built based on the documentation at 
+# https://www.xilinx.com/support/documentation/sw_manuals/xilinx2013_4/ug894-vivado-tcl-scripting.pdf
+
 OUTDIR ?= build
 XDC    ?= src/$(TOP).xdc
 TCL    ?= $(TOP).tcl
-DCP    ?= $(TOP).dcp
 PART   ?= xc7a100tcsg324-1 # For the Nexys4DDR board.
 ENV    ?= /opt/Xilinx/Vivado/2017.2/settings64.sh
 
@@ -9,16 +11,48 @@ all: synth
 
 # Generate tcl-file for vivado batch mode
 $(TCL) : $(SRC) Makefile
-	echo "set outputDir $(OUTDIR)" > $(TCL)
+	cat ../reportCriticalPaths.tcl > $(TCL)
+	# Step 1
+	echo "# Step 1" >> $(TCL)
+	echo "set outputDir $(OUTDIR)" >> $(TCL)
 	echo "file mkdir \$$outputDir" >> $(TCL)
+	# Step 2
+	echo "# Step 2" >> $(TCL)
 	echo "read_vhdl -library xil_defaultlib {" >> $(TCL)
 	for file in $(SRC); do          \
 			echo "  $$file" >> $(TCL); \
 	done;
 	echo "}" >> $(TCL)
 	echo "read_xdc $(XDC)" >> $(TCL)
+	# Step 3
+	echo "# Step 3" >> $(TCL)
 	echo "synth_design -top $(TOP) -part $(PART)" >> $(TCL)
-	echo "write_checkpoint -force $(DCP)" >> $(TCL)
+	echo "write_checkpoint -force \$$outputDir/post_synth.dcp" >> $(TCL)
+	echo "report_timing_summary -file \$$outputDir/post_synth_timing_summary.rpt" >> $(TCL)
+	echo "report_utilization -file \$$outputDir/post_synth_util.rpt" >> $(TCL)
+	echo "reportCriticalPaths \$$outputDir/post_synth_critpath_report.csv" >> $(TCL)
+	# Step 4
+	echo "# Step 4" >> $(TCL)
+	echo "opt_design" >> $(TCL)
+	echo "reportCriticalPaths \$$outputDir/post_opt_critpath_report.csv" >> $(TCL)
+	echo "place_design" >> $(TCL)
+	echo "report_clock_utilization -file \$$outputDir/clock_util.rpt" >> $(TCL)
+	echo "phys_opt_design" >> $(TCL)
+	echo "write_checkpoint -force \$$outputDir/post_place.dcp" >> $(TCL)
+	echo "report_utilization -file \$$outputDir/post_place_util.rpt" >> $(TCL)
+	echo "report_timing_summary -file \$$outputDir/post_place_timing_summary.rpt" >> $(TCL)
+	# Step 5
+	echo "# Step 5" >> $(TCL)
+	echo "route_design" >> $(TCL)
+	echo "write_checkpoint -force \$$outputDir/post_route.dcp" >> $(TCL)
+	echo "report_route_status -file \$$outputDir/post_route_status.rpt" >> $(TCL)
+	echo "report_timing_summary -file \$$outputDir/post_route_timing_summary.rpt" >> $(TCL)
+	echo "report_power -file \$$outputDir/post_route_power.rpt" >> $(TCL)
+	echo "report_drc -file \$$outputDir/post_imp_drc.rpt" >> $(TCL)
+	echo "write_vhdl -force \$$outputDir/impl_netlist.vhdl" >> $(TCL)
+	# Step 6
+	echo "# Step 6" >> $(TCL)
+	echo "write_bitstream -force \$$outputDir/$(TOP).bit" >> $(TCL)
 	echo "exit" >> $(TCL)
 junk += $(TCL)
 
@@ -35,3 +69,4 @@ junk += $(DCP)
 .PHONY: clean
 clean::
 	rm -r -f $(junk)
+
