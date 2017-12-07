9f71248489348b5e33ac2e0fbedb82f46da86a9c
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-12-07 21:41:25 +0100

    Increased frequency to 50 MHz.

--- beta/src/clock/clk_wiz_cpu_clk_wiz.vhd
+++ beta/src/clock/clk_wiz_cpu_clk_wiz.vhd
@@ -55,7 +55,7 @@
 --  Output     Output      Phase    Duty Cycle   Pk-to-Pk     Phase
 --   Clock     Freq (MHz)  (degrees)    (%)     Jitter (ps)  Error (ps)
 ------------------------------------------------------------------------------
--- CLK_OUT1___10.000______0.000______50.0______298.478_____133.882
+-- CLK_OUT1___50.000______0.000______50.0______159.371_____98.575
 --
 ------------------------------------------------------------------------------
 -- Input Clock   Freq (MHz)    Input Jitter (UI)
@@ -128,11 +128,11 @@ clk_in1_clk_wiz_0 <= clk_in1;
     CLKOUT4_CASCADE      => FALSE,
     COMPENSATION         => "ZHOLD",
     STARTUP_WAIT         => FALSE,
-    DIVCLK_DIVIDE        => 2,
-    CLKFBOUT_MULT_F      => 15.625,
+    DIVCLK_DIVIDE        => 1,
+    CLKFBOUT_MULT_F      => 10.000,
     CLKFBOUT_PHASE       => 0.000,
     CLKFBOUT_USE_FINE_PS => FALSE,
-    CLKOUT0_DIVIDE_F     => 78.125,
+    CLKOUT0_DIVIDE_F     => 20.000,
     CLKOUT0_PHASE        => 0.000,
     CLKOUT0_DUTY_CYCLE   => 0.500,
     CLKOUT0_USE_FINE_PS  => FALSE,
