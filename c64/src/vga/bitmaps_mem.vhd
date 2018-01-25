53e5957bb62b871080d7bce2379a974f21e7ba04
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-01-25 20:44:08 +0100

    Old implementation used registers. Now uses BRAM.

--- c64/src/vga/bitmaps_mem.vhd
+++ c64/src/vga/bitmaps_mem.vhd
@@ -37,13 +37,18 @@ architecture Behavioral of bitmaps_mem is
       return res;
    end function reverse;
 
-   -- Bitmaps are stored in BRAM, with 2 address bits to select sprite, and 4 address bits
-   -- to select row in sprite (0 - 15).
-   type t_bitmaps is array (0 to 63) of std_logic_vector(15 downto 0);
+   -- Bitmaps are stored in two parallel BRAMs, with 2 address bits to select
+   -- sprite, and 4 address bits to select row in sprite (0 - 15).
+   type t_bitmaps is array (0 to 63) of std_logic_vector(7 downto 0);
 
-   signal bitmaps : t_bitmaps := (others => (others => '0'));  -- Bitmap data
+   signal bitmaps_lo : t_bitmaps := (others => (others => '0'));  -- Bitmap data
+   signal bitmaps_hi : t_bitmaps := (others => (others => '0'));  -- Bitmap data
 
-   signal cpu_data : std_logic_vector(7 downto 0);
+   signal cpu_data_hi : std_logic_vector(7 downto 0);
+   signal cpu_data_lo : std_logic_vector(7 downto 0);
+
+   signal vga_data_hi : std_logic_vector(7 downto 0);
+   signal vga_data_lo : std_logic_vector(7 downto 0);
 
 begin
 
@@ -51,9 +56,12 @@ begin
    process (vga_clk_i)
    begin
       if rising_edge(vga_clk_i) then
-         vga_data_o <= bitmaps(conv_integer(vga_addr_i));
+         vga_data_hi <= bitmaps_hi(conv_integer(vga_addr_i));
+         vga_data_lo <= bitmaps_lo(conv_integer(vga_addr_i));
       end if;
    end process;
+
+   vga_data_o <= reverse(vga_data_hi) & reverse(vga_data_lo);
 
 
    -- CPU port
@@ -65,8 +73,8 @@ begin
 
          if cpu_wren_i = '1' and cpu_addr_i(8 downto 7) = "00" then
             case cpu_addr_i(0) is
-               when '0' => bitmaps(index_v)( 7 downto 0) <= reverse(cpu_data_i);
-               when '1' => bitmaps(index_v)(15 downto 8) <= reverse(cpu_data_i);
+               when '0' => bitmaps_lo(index_v) <= cpu_data_i;
+               when '1' => bitmaps_hi(index_v) <= cpu_data_i;
                when others => null;
             end case;
          end if;
@@ -77,20 +85,17 @@ begin
       variable index_v : integer range 0 to 63;
    begin
       if falling_edge(cpu_clk_i) then
-         cpu_data <= (others => '0');
          index_v := conv_integer(cpu_addr_i(6 downto 1));
 
          if cpu_rden_i = '1' and cpu_addr_i(8 downto 7) = "00" then
-            case cpu_addr_i(0) is
-               when '0' => cpu_data <= bitmaps(index_v)( 7 downto 0);
-               when '1' => cpu_data <= bitmaps(index_v)(15 downto 8);
-               when others => null;
-            end case;
+            cpu_data_lo <= bitmaps_lo(index_v);
+            cpu_data_hi <= bitmaps_hi(index_v);
          end if;
       end if;
    end process;
 
-   cpu_data_o <= reverse(cpu_data);
+   cpu_data_o <= cpu_data_hi when cpu_addr_i(0) = '1' else
+                 cpu_data_lo;
 
 end Behavioral;
 
