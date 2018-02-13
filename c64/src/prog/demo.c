f526e7248a7a4b0dda125b94f6a7339243472a7e
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-02-13 20:32:21 +0100

    First version.

---
+++ c64/src/prog/demo.c
@@ -0,0 +1,143 @@
+//
+// This is a complete demo. Features demonstrated are:
+// Horizontal scrolling text.
+// Keyboard input.
+// Moving sprites.
+//
+
+#include "memorymap.h"
+
+#define COL_LIGHT       0x6F   // 011_011_11
+#define COL_DARK        0x44   // 010_001_00
+#define COL_BLACK       0x00   // 000_000_00
+
+/*
+ * Zero page addresses
+ */
+#define SCREEN_POS_LO   0x00
+#define SCREEN_POS_HI   0x01
+
+// Forward declarations.
+void __fastcall__ readFromKeyboard(void);
+
+static void __fastcall__ clearScreen(void)
+{
+   // Clear the screen
+   __asm__("LDA #$00"); 
+   __asm__("STA %b", SCREEN_POS_LO); 
+   __asm__("LDA #$80"); 
+   __asm__("STA %b", SCREEN_POS_HI); 
+   __asm__("LDA #$00"); 
+   __asm__("TAX"); 
+clear:
+   __asm__("LDA #$20"); 
+   __asm__("STA (%b, X)", SCREEN_POS_LO); 
+   __asm__("LDA %b", SCREEN_POS_LO); 
+   __asm__("CLC"); 
+   __asm__("ADC #$01"); 
+   __asm__("STA %b", SCREEN_POS_LO); 
+   __asm__("BCC %g", clear); 
+   __asm__("LDA %b", SCREEN_POS_HI); 
+   __asm__("ADC #$00"); 
+   __asm__("STA %b", SCREEN_POS_HI); 
+   __asm__("CMP #$84"); 
+   __asm__("BNE %g", clear); 
+   __asm__("RTS"); 
+} // end of clearScreen
+
+static unsigned char irqA;
+static unsigned char irqX;
+static unsigned char irqCnt;
+
+// The interrupt service routine.
+void __fastcall__ irq(void)
+{
+   __asm__("STA %v", irqA);    // Store A register
+   __asm__("TXA");
+   __asm__("STA %v", irqX);    // Store X register
+
+   __asm__("LDA %w", VGA_ADDR_IRQ);  // Clear IRQ assertion.
+
+   // Blink with cursor
+   __asm__("LDA %v", irqCnt);
+   __asm__("CLC");
+   __asm__("ADC #$04");
+   __asm__("STA %v", irqCnt);
+   __asm__("BNE %g", end_irq);
+
+   __asm__("LDX #$00");
+   __asm__("LDA (%b, X)", SCREEN_POS_LO); 
+   __asm__("EOR #$20");
+   __asm__("STA (%b, X)", SCREEN_POS_LO); 
+
+end_irq:
+   __asm__("LDA %v", irqX);    // Restore X
+   __asm__("TAX");
+   __asm__("LDA %v", irqA);    // Restore A
+
+   __asm__("RTI");
+} // end of irq
+
+
+// Entry point after CPU reset
+void __fastcall__ reset(void)
+{
+   __asm__("SEI");                           // Disable all interrupts
+   __asm__("LDX #$FF");
+   __asm__("TXS");                           // Reset stack pointer
+
+   // Configure text color
+   __asm__("LDA #%b", COL_LIGHT);
+   __asm__("STA %w",  VGA_ADDR_FGCOL);
+   __asm__("LDA #%b", COL_DARK);
+   __asm__("STA %w",  VGA_ADDR_BGCOL);
+   
+   __asm__("JSR %v", clearScreen);
+
+   // Configure VGA interrupt
+   __asm__("LDA #$F0"); 
+   __asm__("STA %w", VGA_ADDR_YLINE);             // Set the interrupt at past the end of the screen
+   __asm__("LDA #$01"); 
+   __asm__("STA %w", VGA_ADDR_MASK);
+   __asm__("LDA %w", VGA_ADDR_IRQ);               // Clear any pending IRQ
+   __asm__("LDA #$00"); 
+   __asm__("STA %v", irqCnt);
+   __asm__("CLI"); 
+
+   // Reset the current screen pointer
+   __asm__("LDA #$00"); 
+   __asm__("STA %b", SCREEN_POS_LO); 
+   __asm__("LDA #$80"); 
+   __asm__("STA %b", SCREEN_POS_HI); 
+
+loop:
+   __asm__("JSR %v", readFromKeyboard);      // This is a blocking call; it won't return until a valid keypress is detected.
+
+   // Now write it to the screen
+   __asm__("LDX #$00"); 
+   __asm__("STA (%b, X)", SCREEN_POS_LO); 
+
+   // Update screen pointer
+   __asm__("LDA %b", SCREEN_POS_LO); 
+   __asm__("CLC"); 
+   __asm__("ADC #$01"); 
+   __asm__("STA %b", SCREEN_POS_LO); 
+   __asm__("LDA %b", SCREEN_POS_HI); 
+   __asm__("ADC #$00"); 
+   __asm__("STA %b", SCREEN_POS_HI); 
+   __asm__("CMP #$84"); 
+   __asm__("BNE %g", noWrap); 
+   __asm__("LDA #$80"); 
+   __asm__("STA %b", SCREEN_POS_HI); 
+noWrap:
+
+   goto loop;  // Just do an endless loop.
+} // end of reset
+
+// Non-maskable interrupt
+void __fastcall__ nmi(void)
+{
+   // Not used.
+   __asm__("RTI");
+} // end of nmi
+
