fe8ccf026fb46de4067871458039332b37c0aaa7
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-02-18 13:08:05 +0100

    Bugfix: Use a BUFGMUX instead of a regular mux, to improve timing.

--- c64/src/clock/clk_rst.vhd
+++ c64/src/clock/clk_rst.vhd
@@ -2,6 +2,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
+library UNISIM;
+use UNISIM.Vcomponents.all;
+
 -- This module generates the clock and reset signals.
 
 entity clk_rst is
@@ -47,7 +50,14 @@ begin
    -- Drive output signals
    -----------------------
 
-   clk_o <= clk_i when clk_mode = '0' else clk_step;
+   -- Note: For some reason, synthesis fails if I0 and I1 are swapped.
+   inst_bufgmux : BUFGMUX
+   port map (
+      I1 => clk_i,
+      I0 => clk_step,
+      S  => clk_mode,
+      O  => clk_o
+   );
 
 end architecture Structural;
 
