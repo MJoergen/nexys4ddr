36c090d0352ac8bfe1f170a09e412b27160f012c
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-03-04 21:00:45 +0100

    Print packet number as well.

--- c64/rx.py
+++ c64/rx.py
@@ -9,6 +9,8 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+cnt = 0
 while True:
+    cnt += 1
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data.encode("hex")
+    print "received message:", cnt, ":", data.encode("hex")
