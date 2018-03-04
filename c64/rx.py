2b3919190b5a514efc938f3fc0ac9cf9beda39b7
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-03-04 12:21:38 +0100

    First version.

---
+++ c64/rx.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import socket
+import pprint
+
+UDP_IP = "192.168.1.43"
+UDP_PORT = 4660 
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print "received message:", data.encode("hex")
