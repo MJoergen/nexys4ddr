4b3f15d868de62a61619ee29ed8bff076059371e
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-04-24 22:01:25 +0200

    Bugfixes.

--- c64/src/prog/tennis_ai.c
+++ c64/src/prog/tennis_ai.c
@@ -21,10 +21,7 @@ ret:
    __asm__("RTS");
 
 moveLeft:
-   // Can we move left?
-   __asm__("CMP #%b", (AI_LEFT_MARGIN-AI_VEL)/2);
-   __asm__("BCC %g", ret);
-
+   // Mode to the left
    __asm__("LDA %w", VGA_ADDR_SPRITE_2_X);
    __asm__("SEC");
    __asm__("SBC #%b", AI_VEL);
@@ -32,13 +29,25 @@ moveLeft:
    __asm__("LDA %w", VGA_ADDR_SPRITE_2_X_MSB);
    __asm__("SBC #$00");
    __asm__("STA %w", VGA_ADDR_SPRITE_2_X_MSB);
+
+   // Have we passed the wall?
+   __asm__("LDA %w", VGA_ADDR_SPRITE_2_X);
+   __asm__("SEC");
+   __asm__("SBC #<%w", AI_LEFT_MARGIN);
+   __asm__("LDA %w", VGA_ADDR_SPRITE_2_X_MSB);
+   __asm__("SBC #>%w", AI_LEFT_MARGIN);
+   __asm__("BCS %g", ret);
+
+   // Snap to wall
+   __asm__("LDA #<%w", AI_LEFT_MARGIN);
+   __asm__("STA %w", VGA_ADDR_SPRITE_2_X);
+   __asm__("LDA #>%w", AI_LEFT_MARGIN);
+   __asm__("STA %w", VGA_ADDR_SPRITE_2_X_MSB);
+   
    __asm__("RTS");
 
 moveRight:
-   // Can we move right?
-   __asm__("CMP #%b", (AI_RIGHT_MARGIN+AI_VEL)/2);
-   __asm__("BCS %g", ret);
-
+   // Mode to the right
    __asm__("LDA %w", VGA_ADDR_SPRITE_2_X);
    __asm__("CLC");
    __asm__("ADC #%b", AI_VEL);
@@ -46,6 +55,21 @@ moveRight:
    __asm__("LDA %w", VGA_ADDR_SPRITE_2_X_MSB);
    __asm__("ADC #$00");
    __asm__("STA %w", VGA_ADDR_SPRITE_2_X_MSB);
+
+   // Have we passed the wall?
+   __asm__("LDA %w", VGA_ADDR_SPRITE_2_X);
+   __asm__("SEC");
+   __asm__("SBC #<%w", AI_RIGHT_MARGIN);
+   __asm__("LDA %w", VGA_ADDR_SPRITE_2_X_MSB);
+   __asm__("SBC #>%w", AI_RIGHT_MARGIN);
+   __asm__("BCC %g", ret);
+
+   // Snap to wall
+   __asm__("LDA #<%w", AI_RIGHT_MARGIN);
+   __asm__("STA %w", VGA_ADDR_SPRITE_2_X);
+   __asm__("LDA #>%w", AI_RIGHT_MARGIN);
+   __asm__("STA %w", VGA_ADDR_SPRITE_2_X_MSB);
+   
    __asm__("RTS");
 
 } // end of ai_move
