8e46a50e6b55b28db0a6c4533c969205414c8297
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-04-27 12:21:25 +0200

    Added colours to show queens capturing each other.

--- c64/src/prog/queens.c
+++ c64/src/prog/queens.c
@@ -6,10 +6,11 @@
 #include "zeropage.h"   // Variables to be stored in the zero-page.
 #include "keyboard.h"
 
-#define COL_WHITE       0xFF   // 111_111_11
-#define COL_LIGHT       0x6F   // 011_011_11
-#define COL_DARK        0x44   // 010_001_00
-#define COL_BLACK       0x00   // 000_000_00
+#define COL_WHITE       0xFFU   // 111_111_11
+#define COL_LIGHT       0x6FU   // 011_011_11
+#define COL_DARK        0x44U   // 010_001_00
+#define COL_BLACK       0x00U   // 000_000_00
+#define COL_RED         0xE0U   // 111_000_00
 
 #define HELP_POS_X      3
 #define HELP_POS_Y      11
@@ -48,6 +49,7 @@ static char lastKey;
 
 static char tempLo;
 static char tempHi;
+static char temp;
 
 static void __fastcall__ my_memcpy(void)
 {
@@ -122,24 +124,48 @@ static void __fastcall__ printStepMode(void)
 static void __fastcall__ clearScreen(void)
 {
    // Clear the screen
-   __asm__("LDA #$00"); 
-   __asm__("STA %b", ZP_SCREEN_POS_LO); 
-   __asm__("LDA #$80"); 
-   __asm__("STA %b", ZP_SCREEN_POS_HI); 
+   __asm__("LDA #<%w", MEM_DISP); 
+   __asm__("STA %b", ZP_DST_LO); 
+   __asm__("LDA #>%w", MEM_DISP); 
+   __asm__("STA %b", ZP_DST_HI); 
    __asm__("LDA #$00"); 
    __asm__("TAY"); 
-clear:
+clear2:
    __asm__("LDA #$20"); 
-   __asm__("STA (%b),Y", ZP_SCREEN_POS_LO); 
+clear1:
+   __asm__("STA (%b),Y", ZP_DST_LO); 
    __asm__("INY"); 
-   __asm__("BNE %g", clear); 
-   __asm__("LDA %b", ZP_SCREEN_POS_HI); 
+   __asm__("BNE %g", clear1); 
+   __asm__("LDA %b", ZP_DST_HI); 
    __asm__("CLC"); 
    __asm__("ADC #$01"); 
-   __asm__("STA %b", ZP_SCREEN_POS_HI); 
-   __asm__("CMP #$84"); 
-   __asm__("BNE %g", clear); 
+   __asm__("STA %b", ZP_DST_HI); 
+   __asm__("CMP #>%w", MEM_DISP+4*256); 
+   __asm__("BNE %g", clear2); 
 } // end of clearScreen
+
+static void __fastcall__ clearColours(void)
+{
+   // Clear the screen
+   __asm__("LDA #<%w", MEM_COL); 
+   __asm__("STA %b", ZP_DST_LO); 
+   __asm__("LDA #>%w", MEM_COL); 
+   __asm__("STA %b", ZP_DST_HI); 
+   __asm__("LDA #$00"); 
+   __asm__("TAY"); 
+clear2:
+   __asm__("LDA #%b", COL_WHITE); 
+clear1:
+   __asm__("STA (%b),Y", ZP_DST_LO); 
+   __asm__("INY"); 
+   __asm__("BNE %g", clear1); 
+   __asm__("LDA %b", ZP_DST_HI); 
+   __asm__("CLC"); 
+   __asm__("ADC #$01"); 
+   __asm__("STA %b", ZP_DST_HI); 
+   __asm__("CMP #>%w", MEM_COL+4*256); 
+   __asm__("BNE %g", clear2); 
+} // end of clearColours
 
 static void printBoard()
 {
@@ -194,34 +220,162 @@ nextRow:
 
 } // end of printBoard
 
-// Prints a value in register 'A' as a two-digit hex number
-static void printHex()
+static void printIllegal()
 {
-   __asm__("ROR A"); 
-   __asm__("ROR A"); 
-   __asm__("ROR A"); 
-   __asm__("ROR A"); 
-   __asm__("AND #$0F"); 
-   __asm__("CMP #$0A"); 
-   __asm__("BCC %g", dig); 
-   __asm__("CLC"); 
-   __asm__("ADC #$07"); 
-dig:
-   __asm__("CLC"); 
-   __asm__("ADC #$30"); 
-   __asm__("STA %w", MEM_DISP+10); 
+   __asm__("LDA %v", curIndex);
+   __asm__("CMP #$08");
+   __asm__("BNE %g", cont);
+   __asm__("RTS");
 
-   __asm__("LDA %v", solutions); 
-   __asm__("AND #$0F"); 
-   __asm__("CMP #$0A"); 
-   __asm__("BCC %g", dig2); 
+cont:
+   __asm__("TAX");
+   __asm__("LDA %v,X", positions);
+   __asm__("TAY");
+checkLegal:
+   __asm__("DEX");
+   __asm__("BMI %g", checkDiagonal);
+   __asm__("CMP %v,X", positions);
+   __asm__("BNE %g", checkLegal);
+   // Illegal vertical. Register 'X' contains the top row, register 'Y' contains the column.
+   __asm__("LDA #<%w", MEM_COL); 
+   __asm__("STA %b", ZP_SCREEN_POS_LO); 
+   __asm__("LDA #>%w", MEM_COL); 
+   __asm__("STA %b", ZP_SCREEN_POS_HI); 
+   __asm__("STX %v", temp);
+   __asm__("TXA");
+   __asm__("BEQ %g", draw);
+nextRow:
+   __asm__("LDA %b", ZP_SCREEN_POS_LO); 
    __asm__("CLC"); 
-   __asm__("ADC #$07"); 
-dig2:
+   __asm__("ADC #$28"); 
+   __asm__("STA %b", ZP_SCREEN_POS_LO); 
+   __asm__("LDA %b", ZP_SCREEN_POS_HI); 
+   __asm__("ADC #$00"); 
+   __asm__("STA %b", ZP_SCREEN_POS_HI); 
+   __asm__("DEX");
+   __asm__("BNE %g", nextRow);
+draw:
+   __asm__("LDA %v", temp);
+   __asm__("TAX");
+colRow:
+   __asm__("LDA #%b", COL_RED);
+   __asm__("STA (%b),Y", ZP_SCREEN_POS_LO); 
+   __asm__("LDA %b", ZP_SCREEN_POS_LO); 
    __asm__("CLC"); 
-   __asm__("ADC #$30"); 
-   __asm__("STA %w", MEM_DISP+11); 
-} // end of printHex
+   __asm__("ADC #$28"); 
+   __asm__("STA %b", ZP_SCREEN_POS_LO); 
+   __asm__("LDA %b", ZP_SCREEN_POS_HI); 
+   __asm__("ADC #$00"); 
+   __asm__("STA %b", ZP_SCREEN_POS_HI); 
+   __asm__("INX");
+   __asm__("TXA");
+   __asm__("CMP %v", curIndex);
+   __asm__("BCC %g", colRow);
+   __asm__("BEQ %g", colRow);
+
+checkDiagonal:
+   __asm__("LDA %v", curIndex);
+   __asm__("TAX");
+   __asm__("LDA %v,X", positions);
+checkLegal2:
+   __asm__("DEX");
+   __asm__("BMI %g", checkDiagonal2);
+   __asm__("SEC");
+   __asm__("SBC #$01");
+   __asm__("CMP %v,X", positions);
+   __asm__("BNE %g", checkLegal2);
+   __asm__("TAY");
+   // Illegal Diagonal. Register 'X' contains the top row, register 'Y' contains the top column.
+   __asm__("LDA #<%w", MEM_COL); 
+   __asm__("STA %b", ZP_SCREEN_POS_LO); 
+   __asm__("LDA #>%w", MEM_COL); 
+   __asm__("STA %b", ZP_SCREEN_POS_HI); 
+   __asm__("STX %v", temp);
+   __asm__("TXA");
+   __asm__("BEQ %g", draw2);
+nextRow2:
+   __asm__("LDA %b", ZP_SCREEN_POS_LO); 
+   __asm__("CLC"); 
+   __asm__("ADC #$28"); 
+   __asm__("STA %b", ZP_SCREEN_POS_LO); 
+   __asm__("LDA %b", ZP_SCREEN_POS_HI); 
+   __asm__("ADC #$00"); 
+   __asm__("STA %b", ZP_SCREEN_POS_HI); 
+   __asm__("DEX");
+   __asm__("BNE %g", nextRow2);
+draw2:
+   __asm__("LDA %v", temp);
+   __asm__("TAX");
+colRow2:
+   __asm__("LDA #%b", COL_RED);
+   __asm__("STA (%b),Y", ZP_SCREEN_POS_LO); 
+   __asm__("LDA %b", ZP_SCREEN_POS_LO); 
+   __asm__("CLC"); 
+   __asm__("ADC #$28"); 
+   __asm__("STA %b", ZP_SCREEN_POS_LO); 
+   __asm__("LDA %b", ZP_SCREEN_POS_HI); 
+   __asm__("ADC #$00"); 
+   __asm__("STA %b", ZP_SCREEN_POS_HI); 
+   __asm__("INX");
+   __asm__("INY");
+   __asm__("TXA");
+   __asm__("CMP %v", curIndex);
+   __asm__("BCC %g", colRow2);
+   __asm__("BEQ %g", colRow2);
+
+checkDiagonal2:
+   __asm__("LDA %v", curIndex);
+   __asm__("TAX");
+   __asm__("LDA %v,X", positions);
+checkLegal3:
+   __asm__("DEX");
+   __asm__("BMI %g", end);
+   __asm__("CLC");
+   __asm__("ADC #$01");
+   __asm__("CMP %v,X", positions);
+   __asm__("BNE %g", checkLegal3);
+   __asm__("TAY");
+   // Illegal Diagonal. Register 'X' contains the top row, register 'Y' contains the top column.
+   __asm__("LDA #<%w", MEM_COL); 
+   __asm__("STA %b", ZP_SCREEN_POS_LO); 
+   __asm__("LDA #>%w", MEM_COL); 
+   __asm__("STA %b", ZP_SCREEN_POS_HI); 
+   __asm__("STX %v", temp);
+   __asm__("TXA");
+   __asm__("BEQ %g", draw3);
+nextRow3:
+   __asm__("LDA %b", ZP_SCREEN_POS_LO); 
+   __asm__("CLC"); 
+   __asm__("ADC #$28"); 
+   __asm__("STA %b", ZP_SCREEN_POS_LO); 
+   __asm__("LDA %b", ZP_SCREEN_POS_HI); 
+   __asm__("ADC #$00"); 
+   __asm__("STA %b", ZP_SCREEN_POS_HI); 
+   __asm__("DEX");
+   __asm__("BNE %g", nextRow3);
+draw3:
+   __asm__("LDA %v", temp);
+   __asm__("TAX");
+colRow3:
+   __asm__("LDA #%b", COL_RED);
+   __asm__("STA (%b),Y", ZP_SCREEN_POS_LO); 
+   __asm__("LDA %b", ZP_SCREEN_POS_LO); 
+   __asm__("CLC"); 
+   __asm__("ADC #$28"); 
+   __asm__("STA %b", ZP_SCREEN_POS_LO); 
+   __asm__("LDA %b", ZP_SCREEN_POS_HI); 
+   __asm__("ADC #$00"); 
+   __asm__("STA %b", ZP_SCREEN_POS_HI); 
+   __asm__("INX");
+   __asm__("DEY");
+   __asm__("TXA");
+   __asm__("CMP %v", curIndex);
+   __asm__("BCC %g", colRow3);
+   __asm__("BEQ %g", colRow3);
+
+end:
+   __asm__("RTS");
+} // end of printIllegal
 
 // Prints a value in register 'A' as a three-digit decimal number
 static void printOneByteDec()
@@ -457,10 +611,6 @@ void __fastcall__ reset(void)
    __asm__("LDX #$FF");
    __asm__("TXS");                           // Reset stack pointer
 
-   clearScreen();
-   printHelp();
-   printStepMode();
-
    // Configure text color
    __asm__("LDA #%b", COL_LIGHT);
    __asm__("STA %w",  VGA_ADDR_FGCOL);
@@ -482,11 +632,23 @@ loop1:
    __asm__("LDA #%b", STEP_MODE_SINGLE);
    __asm__("STA %v", stepMode);
 
+   clearScreen();
+   clearColours();
+   printHelp();
+   printStepMode();
+   printBoard();
+
+   __asm__("LDA %v", solutions); 
+   printOneByteDec();
+
+   __asm__("LDA %v", countHi); 
+   __asm__("TAX"); 
+   __asm__("LDA %v", countLo); 
+   printTwoByteDec();
+
    // Configure VGA interrupt
    __asm__("LDA #$E0"); 
    __asm__("STA %w", VGA_ADDR_YLINE);             // Set the interrupt at the end of the screen
-   __asm__("LDA #$01"); 
-   __asm__("STA %w", VGA_ADDR_MASK);
    __asm__("LDA %w", VGA_ADDR_IRQ);
    __asm__("STA %w", VGA_ADDR_IRQ);               // Clear any pending IRQ
    __asm__("CLI"); 
@@ -537,8 +699,10 @@ void __fastcall__ irq(void)
    __asm__("LDA %w", VGA_ADDR_IRQ);
    __asm__("STA %w", VGA_ADDR_IRQ);                // Clear any pending IRQ
 
-   printBoard();
    updateBoard();
+   printBoard();
+   clearColours();
+   printIllegal();
 
    __asm__("LDA %v", countLo); 
    __asm__("CLC"); 
