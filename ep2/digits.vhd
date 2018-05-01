5faa34e8a47d1cb68b7ad7506800562042fe4309
Michael Jørgensen <mfj@silicom.dk>
2018-05-01 11:18:25 +0200

    Cleanup.

--- ep2/digits.vhd
+++ ep2/digits.vhd
@@ -16,8 +16,13 @@ end digits;
 
 architecture Structural of digits is
 
+   -- Define screen size
+   constant H_PIXELS : integer := 640;
+   constant V_PIXELS : integer := 480;
+
    -- Define colours
    constant COL_BLACK : std_logic_vector(7 downto 0) := B"000_000_00";
+   constant COL_DARK  : std_logic_vector(7 downto 0) := B"001_001_01";
    constant COL_WHITE : std_logic_vector(7 downto 0) := B"111_111_11";
    constant COL_RED   : std_logic_vector(7 downto 0) := B"111_000_00";
    constant COL_GREEN : std_logic_vector(7 downto 0) := B"000_111_00";
@@ -44,13 +49,15 @@ architecture Structural of digits is
       "01111000" &
       "00000000";
 
+   -- Character row and column
+   signal char_col : std_logic_vector(5 downto 0);
+   signal char_row : std_logic_vector(5 downto 0);
+
+   signal pix_row : integer range 0 to 7;
+   signal pix_col : integer range 0 to 7;
+
    -- Pixel colour
    signal vga_col : std_logic_vector(7 downto 0);
-
-   signal row : integer range 0 to 7;
-   signal col : integer range 0 to 7;
-   signal char_x : std_logic_vector(5 downto 0);
-   signal char_y : std_logic_vector(5 downto 0);
 
 begin
 
@@ -59,35 +66,43 @@ begin
    -- Generate pixel colour
    --------------------------------------------------
 
-   char_x <= pix_x_i(9 downto 4);
-   char_y <= pix_y_i(9 downto 4);
-   col    <= 7 - conv_integer(pix_x_i(3 downto 1));
-   row    <= 7 - conv_integer(pix_y_i(3 downto 1));
+   char_col  <= pix_x_i(9 downto 4);
+   char_row  <= pix_y_i(9 downto 4);
+   pix_col <= 7 - conv_integer(pix_x_i(3 downto 1));
+   pix_row <= 7 - conv_integer(pix_y_i(3 downto 1));
 
-   p_col : process (clk_i)
-      variable digit_v  : std_logic;
-      variable offset_v : integer;
-      variable pix_v    : std_logic;
+   p_vga_col : process (clk_i)
+      variable char_num_v : integer;
+      variable digit_v    : std_logic;
+      variable pix_v      : std_logic;
    begin
       if rising_edge(clk_i) then
-         vga_col <= COL_BLACK;
 
-         if char_y = 15 and char_x >= 20 and char_x < 28 then
-            offset_v := conv_integer(char_x)-20;
-            digit_v := digits_i(7-offset_v);
+         -- Set the default background colour
+         vga_col <= COL_DARK;
+
+         if char_row = 15 and char_col >= 20 and char_col < 28 then
+            char_num_v := conv_integer(char_col)-20;
+            digit_v := digits_i(7-char_num_v);
 
             if digit_v = '1' then
-               pix_v := one(row*8+col);
+               pix_v := one(pix_row*8+pix_col);
             else
-               pix_v := zero(row*8+col);
+               pix_v := zero(pix_row*8+pix_col);
             end if;
 
             if pix_v = '1' then
                vga_col <= COL_WHITE;
             end if;
          end if;
+
+         -- Make sure colour is black outside visible screen
+         if pix_x_i >= H_PIXELS or pix_y_i >= V_PIXELS then
+            vga_col <= COL_BLACK;
+         end if;
+
       end if;
-   end process p_col;
+   end process p_vga_col;
 
 
    --------------------------------------------------
