9328e43babbb32be39ccd7bc62c974db4db91e33
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-05-05 08:22:57 +0200

    Added text to output.

--- dyoc/ep5/vga/digits.vhd
+++ dyoc/ep5/vga/digits.vhd
@@ -31,8 +31,14 @@ architecture Structural of digits is
    -- Each character is 16x16 pixels, so the screen contains 40x30 characters.
 
    -- Define positioning of first digit
-   constant DIGITS_CHAR_X : integer := 2;
+   constant DIGITS_CHAR_X : integer := 8;
    constant DIGITS_CHAR_Y : integer := 15;
+
+   constant TEXT_CHAR_X   : integer := 2;
+   constant TEXT_CHAR_Y   : integer := DIGITS_CHAR_Y;
+
+   type txt_t is array (0 to 14) of character;
+   constant txt : txt_t := "DI AR" & "IR CN" & "  PC ";
 
    -- A single character bitmap is defined by 8x8 = 64 bits.
    subtype bitmap_t is std_logic_vector(63 downto 0);
@@ -55,8 +61,11 @@ architecture Structural of digits is
    signal nibble_offset : integer range 0 to 11;
    signal nibble_index  : integer range 0 to 11;
    signal nibble        : std_logic_vector(3 downto 0);
+   signal txt_offset    : integer range 0 to 14;
 
    -- Bitmap of digit at current position
+   signal char_nibble   : std_logic_vector(7 downto 0);
+   signal char_txt      : std_logic_vector(7 downto 0);
    signal char          : std_logic_vector(7 downto 0);
    signal bitmap        : bitmap_t;
 
@@ -83,17 +92,26 @@ begin
    -- Calculate value of digit at current position ('0' or '1')
    --------------------------------------------------
 
-   nibble_offset <= char_col - DIGITS_CHAR_X;
+   nibble_offset <= (char_row - DIGITS_CHAR_Y)*4 + (char_col - DIGITS_CHAR_X);
    nibble_index  <= 11 - nibble_offset;
    nibble        <= digits_i(4*nibble_index+3 downto 4*nibble_index);
+   txt_offset    <= (char_row - TEXT_CHAR_Y)*5 + (char_col - TEXT_CHAR_X);
 
 
    --------------------------------------------------
    -- Calculate character to display at current position
    --------------------------------------------------
 
-   char <= nibble + X"30" when nibble < 10 else
+   char_nibble <= nibble + X"30" when nibble < 10 else
            nibble + X"41" - 10;
+
+   char_txt    <= std_logic_vector(to_unsigned(character'pos(txt(txt_offset)), 8));
+
+   char <= char_nibble when char_row >= DIGITS_CHAR_Y and char_row < DIGITS_CHAR_Y+3 and
+                            char_col >= DIGITS_CHAR_X and char_col < DIGITS_CHAR_X+4 else
+           char_txt    when char_row >= TEXT_CHAR_Y   and char_row < TEXT_CHAR_Y+3 and
+                            char_col >= TEXT_CHAR_X   and char_col < TEXT_CHAR_X+5 else
+           X"20"; -- Space
 
 
    --------------------------------------------------
@@ -131,16 +149,16 @@ begin
          -- Set the default screen background colour
          vga_col <= COL_BLACK;
 
-         -- Are we within the borders of the text?
-         if char_row = DIGITS_CHAR_Y and
-            char_col >= DIGITS_CHAR_X and char_col < DIGITS_CHAR_X+12 then
+--         -- Are we within the borders of the text?
+--         if char_row >= DIGITS_CHAR_Y and char_row < DIGITS_CHAR_Y+3 and
+--            char_col >= DIGITS_CHAR_X and char_col < DIGITS_CHAR_X+4 then
 
             if pix = '1' then
                vga_col <= COL_WHITE;
             else
                vga_col <= COL_DARK; -- Text background colour.
             end if;
-         end if;
+--         end if;
 
          -- Make sure colour is black outside visible screen
          if pix_x_i >= H_PIXELS or pix_y_i >= V_PIXELS then
