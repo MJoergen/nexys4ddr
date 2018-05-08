36acafc25caa8e3270d2782dd3341fece9473735
Michael Jørgensen <mfj@silicom.dk>
2018-05-08 10:11:23 +0200

    Applied changes from previous episodes.

--- dyoc/ep4_-_Hexadecimal/vga/vga.vhd
+++ dyoc/ep4_-_Hexadecimal/vga/vga.vhd
@@ -16,31 +16,50 @@ end vga;
 
 architecture Structural of vga is
 
-   -- VGA signals
-   signal pix_x   : std_logic_vector(9 downto 0);
-   signal pix_y   : std_logic_vector(9 downto 0);
-   signal vga_hs  : std_logic;
-   signal vga_vs  : std_logic;
-   signal vga_col : std_logic_vector(7 downto 0);
+   -- Define constants used for 640x480 @ 60 Hz.
+   -- Requires a clock of 25.175 MHz.
+   -- See page 17 in "VESA MONITOR TIMING STANDARD"
+   -- http://caxapa.ru/thumbs/361638/DMTv1r11.pdf
+   constant H_TOTAL  : integer := 800;
+   constant V_TOTAL  : integer := 525;
+
+   -- Pixel counters
+   signal pix_x : std_logic_vector(9 downto 0) := (others => '0');
+   signal pix_y : std_logic_vector(9 downto 0) := (others => '0');
 
 begin
    
    --------------------------------------------------
-   -- Generate horizontal and vertical sync signals
+   -- Generate horizontal and vertical pixel counters
    --------------------------------------------------
 
-   i_sync : entity work.sync
-   port map (
-      clk_i    => clk_i,
-      pix_x_o  => pix_x,
-      pix_y_o  => pix_y,
-      vga_hs_o => vga_hs,
-      vga_vs_o => vga_vs
-   );
+   p_pix_x : process (clk_i)
+   begin
+      if rising_edge(clk_i) then
+         if pix_x = H_TOTAL-1 then
+            pix_x <= (others => '0');
+         else
+            pix_x <= pix_x + 1;
+         end if;
+      end if;
+   end process p_pix_x;
 
-   
+   p_pix_y : process (clk_i)
+   begin
+      if rising_edge(clk_i) then
+         if pix_x = H_TOTAL-1  then
+            if pix_y = V_TOTAL-1 then
+               pix_y <= (others => '0');
+            else
+               pix_y <= pix_y + 1;
+            end if;
+         end if;
+      end if;
+   end process p_pix_y;
+
+
    --------------------------------------------------
-   -- Generate pixel colour
+   -- Instantiate digits generator
    --------------------------------------------------
 
    i_digits : entity work.digits
@@ -49,20 +68,13 @@ begin
    )
    port map (
       clk_i     => clk_i,
+      digits_i  => digits_i,
       pix_x_i   => pix_x,
       pix_y_i   => pix_y,
-      digits_i  => digits_i,
-      vga_col_o => vga_col
+      vga_hs_o  => vga_hs_o,
+      vga_vs_o  => vga_vs_o,
+      vga_col_o => vga_col_o
    );
-
-
-   --------------------------------------------------
-   -- Drive output signals
-   --------------------------------------------------
-
-   vga_hs_o  <= vga_hs;
-   vga_vs_o  <= vga_vs;
-   vga_col_o <= vga_col;
 
 end architecture Structural;
 
