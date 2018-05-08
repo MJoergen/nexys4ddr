d84b146a4fc0d33165ef957204ba7522f36e862a
Michael Jørgensen <mfj@silicom.dk>
2018-05-08 09:29:30 +0200

    Minor changes.

--- dyoc/ep1_-_Hello_World/comp.tcl
+++ dyoc/ep1_-_Hello_World/comp.tcl
@@ -1,7 +1,7 @@
 # This is a tcl command script for the Vivado tool chain
 read_vhdl comp.vhd
 read_xdc comp.xdc
-synth_design -top comp -part xc7a100tcsg324-1
+synth_design -top comp -part xc7a100tcsg324-1 -flatten_hierarchy none
 place_design
 route_design
 write_checkpoint -force comp.dcp
