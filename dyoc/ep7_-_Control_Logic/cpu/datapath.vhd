86d266e937dc7994a8ff6ea1505b83132595d825
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-05-10 10:22:04 +0200

    Copied from Episode 7.

---
+++ dyoc/ep7_-_Control_Logic/cpu/datapath.vhd
@@ -0,0 +1,131 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity datapath is
+   port (
+      clk_i   : in  std_logic;
+      wait_i  : in  std_logic;
+
+      addr_o  : out std_logic_vector(15 downto 0);
+      data_i  : in  std_logic_vector(7 downto 0);
+      data_o  : out std_logic_vector(7 downto 0);
+      wren_o  : out std_logic;
+
+      ar_sel_i   : in  std_logic;
+      hi_sel_i   : in  std_logic;
+      lo_sel_i   : in  std_logic;
+      pc_sel_i   : in  std_logic_vector(1 downto 0);
+      addr_sel_i : in  std_logic_vector(1 downto 0);
+      data_sel_i : in  std_logic_vector(1 downto 0);
+
+      debug_o : out std_logic_vector(79 downto 0)
+   );
+end entity datapath;
+
+architecture structural of datapath is
+
+   -- Program Counter
+   signal pc : std_logic_vector(15 downto 0) := (others => '0');
+
+   -- 'A' register
+   signal ar : std_logic_vector(7 downto 0);
+
+   -- Adress Hi register
+   signal hi : std_logic_vector(7 downto 0);
+   
+   -- Adress Lo register
+   signal lo : std_logic_vector(7 downto 0);
+
+   signal addr : std_logic_vector(15 downto 0);
+   signal data : std_logic_vector(7 downto 0);
+   signal wren : std_logic;
+   
+begin
+
+   -- Program Counter
+   p_pc : process (clk_i)
+   begin
+      if rising_edge(clk_i) then
+         if wait_i = '0' then
+            case pc_sel_i is
+               when "00" => null;
+               when "01" => pc <= pc + 1;
+               when "10" => pc <= hi & lo;
+               when others => null;
+            end case;
+         end if;
+      end if;
+   end process p_pc;
+
+   -- 'A' register
+   p_ar : process (clk_i)
+   begin
+      if rising_edge(clk_i) then
+         if wait_i = '0' then
+            if ar_sel_i = '1' then
+               ar <= data_i;
+            end if;
+         end if;
+      end if;
+   end process p_ar;
+
+   -- 'Hi' register
+   p_hi : process (clk_i)
+   begin
+      if rising_edge(clk_i) then
+         if wait_i = '0' then
+            if hi_sel_i = '1' then
+               hi <= data_i;
+            end if;
+         end if;
+      end if;
+   end process p_hi;
+
+   -- 'Lo' register
+   p_lo : process (clk_i)
+   begin
+      if rising_edge(clk_i) then
+         if wait_i = '0' then
+            if lo_sel_i = '1' then
+               lo <= data_i;
+            end if;
+         end if;
+      end if;
+   end process p_lo;
+
+
+   -- Output multiplexers
+   addr <= (others => '0') when addr_sel_i = "00" else
+           pc              when addr_sel_i = "01" else
+           hi & lo         when addr_sel_i = "10" else
+           (others => '0');
+
+   data <= (others => '0') when data_sel_i = "00" else
+           ar              when data_sel_i = "01" else
+           (others => '0');
+
+   wren <= '1' when data_sel_i = "01" else
+           '0';
+
+
+   -----------------
+   -- Drive output signals
+   -----------------
+
+   debug_o(15 downto  0) <= pc;     -- Two bytes
+   debug_o(23 downto 16) <= ar;     -- One byte
+   debug_o(31 downto 24) <= data_i; -- One byte
+   debug_o(39 downto 32) <= lo;     -- One byte
+   debug_o(47 downto 40) <= hi;     -- One byte
+   debug_o(63 downto 48) <= addr;   -- Two bytes
+   debug_o(71 downto 64) <= data;   -- One byte
+   debug_o(72)           <= wren;   -- One byte
+   debug_o(79 downto 73) <= (others => '0');
+
+   addr_o <= addr;
+   data_o <= data;
+   wren_o <= wren;
+
+end architecture structural;
+
--- dyoc/ep7_-_Control_Logic/mem/mem.vhd
+++ dyoc/ep7_-_Control_Logic/mem/mem.vhd
@@ -1,70 +1,80 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
-use ieee.numeric_std.all;
+
+-- This module models a single-port asynchronous RAM.
+-- Even though there are separate signals for data
+-- input and data output, simultaneous read and write
+-- will not be used in this design.
+--
+-- Data read is present half way through the same clock cycle.
+-- This is done by using a synchronous Block RAM, and reading
+-- on the *falling* edge of the clock cycle.
 
 entity mem is
    generic (
+      -- Number of bits in the address bus. The size of the memory will
+      -- be 2**G_ADDR_BITS bytes.
       G_ADDR_BITS : integer
    );
    port (
       clk_i  : in  std_logic;
+
+      -- Current address selected.
       addr_i : in  std_logic_vector(G_ADDR_BITS-1 downto 0);
-      wren_i : in  std_logic;
+
+      -- Data contents at the selected address.
+      -- Valid in same clock cycle.
+      data_o : out std_logic_vector(7 downto 0);
+
+      -- New data to (optionally) be written to the selected address.
       data_i : in  std_logic_vector(7 downto 0);
-      data_o : out std_logic_vector(7 downto 0)
+
+      -- '1' indicates we wish to perform a write at the selected address.
+      wren_i : in  std_logic
    );
 end mem;
 
 architecture Structural of mem is
 
-   -- Just 2 kBytes of memory.
+   -- This defines a type containing an array of bytes
    type mem_t is array (0 to 2**G_ADDR_BITS-1) of std_logic_vector(7 downto 0);
 
    -- Initialize memory contents
    signal mem : mem_t := (
-      X"A9",   -- LDA #
-      X"07",
+      X"A9", X"01",           -- LDA #$01
+      X"8D", X"0F", X"00",    -- STA $000F
+      X"A9", X"02",           -- LDA #$02
+      X"AD", X"0F", X"00",    -- LDA $000F
+      X"4C", X"02", X"00",    -- JMP $0002
 
-      X"AD",   -- LDA a
-      X"02",
-      X"00",
+      others => X"00"
+   );
 
-      X"8D",   -- STA a
-      X"08",
-      X"00",
-
-      X"8D",   -- STA a
-      X"08",
-      X"00",
-
-      X"4C",   -- JMP a
-      X"02",
-      X"00",
-
-      others => X"00");
-
+   -- Data read from memory.
    signal data : std_logic_vector(7 downto 0);
 
 begin
 
    -- Write process
-   process (clk_i)
+   p_mem : process (clk_i)
    begin
       if rising_edge(clk_i) then
          if wren_i = '1' then
             mem(conv_integer(addr_i)) <= data_i;
          end if;
       end if;
-   end process;
+   end process p_mem;
 
-   -- Read process
-   process (clk_i)
+   -- Read process.
+   -- Triggered on the *falling* clock edge in order to mimick an asynchronous
+   -- memory.
+   p_data : process (clk_i)
    begin
       if falling_edge(clk_i) then
          data <= mem(conv_integer(addr_i));
       end if;
-   end process;
+   end process p_data;
 
    -- Drive output signals
    data_o <= data;
