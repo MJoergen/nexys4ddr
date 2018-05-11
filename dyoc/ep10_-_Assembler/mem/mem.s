c8794c460cf9de12478fbf628553681d979d8442
Michael Jørgensen <mfj@silicom.dk>
2018-05-11 15:38:28 +0200

    Added first version of CPU test.

--- dyoc/ep10_-_Assembler/mem/mem.s
+++ dyoc/ep10_-_Assembler/mem/mem.s
@@ -1,29 +1,108 @@
 	.setcpu		"6502"
 
 .segment	"CODE"
-   LDA #$0A
-   STA tmp
+
+; Test S and Z when loading $00.
+; Conditional branch forward
    LDA #$00
-   STA sum
+   BNE error1     ; Should not jump
+   BMI error1     ; Should not jump
+   BEQ noError1   ; Should jump
+error1:
+   LDA #$01
+   JMP error1
+noError1:
+   BPL noError1a  ; Should jump
+   JMP error1
+noError1a:
 
-loop:
-   LDA sum
+; Test S and Z when loading $01.
+; Conditional branch forward
+   LDA #$FF
+   BEQ error2     ; Should not jump
+   BPL error2     ; Should not jump
+   BNE noError2   ; Should jump
+error2:
+   LDA #$02
+   JMP error2
+noError2:
+   BMI noError2a  ; Should jump
+   JMP error2
+noError2a:
+
+; Test C.
+; Conditional branch forward
    CLC
-   ADC tmp
-   STA sum
-
-   LDA tmp
+   BCS error3     ; Should not jump
+   BCC noError3   ; Should jump
+error3:
+   LDA #$03
+   JMP error3
+noError3:
    SEC
-   SBC #$01
-   STA tmp
+   BCC error3     ; Should not jump
+   BCS noError4   ; Should jump
+   JMP error3
 
-   BNE loop
+; Next we test conditional branching backward
+noError4c:
+   LDA #$FF
+   BNE noError4e  ; Should jump
+   JMP error4d
+noError4b:
+   LDA #$00
+   BEQ noError4c  ; Should jump
+   JMP error4d
+noError4a:
+   SEC
+   LDA #$00       ; Verify carry is not altered
+   BCS noError4b  ; Should jump
+   JMP error4d
+noError4:
+   CLC
+   LDA #$00       ; Verify carry is not altered
+   BCC noError4a  ; Should jump
+   JMP error4d
+error4d:
+   LDA #$04
+   JMP error4d
+noError4e:
 
-   LDA sum
-end:
-   JMP end
+; Now we test compare
+   SEC            ; Preset. Should be cleared in compare.
+   LDA #$55
+   CMP #$AA
+   BCS error5a    ; Should not jump
+   BCC noError5a  ; Should jump
+error5a:
+   LDA #$05
+   JMP error5a
+noError5a:
+   BEQ error5a    ; Should not jump
+   BPL error5a    ; Should not jump
 
-.ORG $03FE  ; Make sure the two variables are placed at the end of the memory.
+   CLC            ; Preclear. Should be set in compare.
+   CMP #$55       ; Verify value in A register is unchanged.
+   BCC error5c    ; Should not jump
+   BCS noError5c  ; Should jump
+error5c:
+   LDA #$05
+   JMP error5c
+noError5c:
+   BNE error5c    ; Should not jump
+   BMI error5c    ; Should not jump
 
-sum: .byte $00
-tmp: .byte $00
+   CLC            ; Preclear. Should be set in compare.
+   CMP #$33
+   BCC error5b    ; Should not jump
+   BCS noError5b  ; Should jump
+error5b:
+   JMP error5b
+noError5b:
+   BEQ error5b    ; Should not jump
+   BMI error5b    ; Should not jump
+
+success:
+   LDA #$FF
+   JMP success
+
