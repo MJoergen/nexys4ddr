ee6d6d9d169bcdd384c5313f0ef8bd8e63b9530f
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-05-28 05:16:56 +0200

    Cleanup.

--- dyoc/ep04_-_Hexadecimal/comp.tcl
+++ dyoc/ep04_-_Hexadecimal/comp.tcl
@@ -1,5 +1,7 @@
 # This is a tcl command script for the Vivado tool chain
-read_vhdl {comp.vhd vga/font.vhd vga/digits.vhd vga/vga.vhd mem/mem.vhd}
+read_vhdl {comp.vhd \
+   vga/font.vhd vga/digits.vhd vga/vga.vhd \
+   mem/mem.vhd}
 read_xdc comp.xdc
 synth_design -top comp -part xc7a100tcsg324-1 -flatten_hierarchy none
 place_design
