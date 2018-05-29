9ecf2c30069e72ff762facf27e78c84085062a28
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-05-29 20:43:42 +0200

    Cleanup.

--- dyoc/ep19_-_C_Runtime/mem/mem.vhd
+++ dyoc/ep19_-_C_Runtime/mem/mem.vhd
@@ -32,10 +32,13 @@ architecture Structural of mem is
 
    signal rom_wren : std_logic;
    signal rom_data : std_logic_vector(7 downto 0);
+   signal rom_cs   : std_logic;
    signal ram_wren : std_logic;
    signal ram_data : std_logic_vector(7 downto 0);
+   signal ram_cs   : std_logic;
    signal cic_wren : std_logic;
    signal cic_data : std_logic_vector(7 downto 0);
+   signal cic_cs   : std_logic;
 
 begin
 
@@ -92,16 +95,22 @@ begin
    -- Address decoding
    ----------------------
 
-   rom_wren <= wren_i when addr_i(15 downto 11) = "11111" else
-               '0';
-   ram_wren <= wren_i when addr_i(15 downto 12) = "0000"  else
-               '0';
-   cic_wren <= wren_i when addr_i = X"BFFF"               else
-               '0';
+   rom_cs <= '1' when addr_i(15 downto 11) = "11111" else
+             '0';
 
-   data_o <= rom_data when addr_i(15 downto 11) = "11111" else
-             ram_data when addr_i(15 downto 12) = "0000"  else
-             cic_data when addr_i = X"BFFF"               else
+   ram_cs <= '1' when addr_i(15 downto 12) = "0000"  else
+             '0';
+
+   cic_cs <= '1' when addr_i = X"BFFF"               else
+             '0';
+
+   rom_wren <= wren_i and rom_cs;
+   ram_wren <= wren_i and ram_cs;
+   cic_wren <= wren_i and cic_cs;
+
+   data_o <= rom_data when rom_cs = '1' else
+             ram_data when ram_cs = '1' else
+             cic_data when cic_cs = '1' else
              X"00";
   
 end Structural;
