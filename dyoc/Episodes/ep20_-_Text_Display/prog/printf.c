c68752d3220d32c2b29f18b507c4a66f3fc7bfc0
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-05-31 22:44:07 +0200

    Bugfix: Would only print 0's.

--- dyoc/Episodes/ep20_-_Text_Display/prog/printf.c
+++ dyoc/Episodes/ep20_-_Text_Display/prog/printf.c
@@ -41,7 +41,7 @@ void printf(char* str)
 
 void printfHex(uint8_t key)
 {
-    char* foo = "00";
+    char foo[3] = "00";
     char* hex = "0123456789ABCDEF";
     foo[0] = hex[(key >> 4) & 0xF];
     foo[1] = hex[key & 0xF];
