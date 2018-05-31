4ee438d023bc0185f08c8ffacf277b24df4ff035
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-05-31 05:44:50 +0200

    Copied from Episode 19.

---
+++ dyoc/ep20_-_Text_Display/README.md
@@ -0,0 +1,6 @@
+# Design Your Own Computer
+# Episode 20 : "Text Display"
+
+Welcome to "Design Your Own Computer".  After this episode
+the computer will be able to display 80x60 characters on the VGA output.
+
---
+++ dyoc/ep20_-_Text_Display/prog/rom.c
@@ -0,0 +1,58 @@
+// A little program to do some array manipulations. This is just a simple test of the toolchain and the CPU implementation.
+// Call first the rom_init() function to initialize.
+// After 100 calls to rom_iter() the m_mem[] array should contain the following:
+// 1 7 7 2 1 2 1 2 0 2
+// and the m_idx variable should contain zero.
+
+#include <stdint.h>
+#include "rom.h"
+
+void rom_init(t_rom *ptr)
+{
+   uint8_t i;
+
+   for (i=0; i<SIZE; ++i)
+      ptr->m_mem[i] = 0;
+
+   ptr->m_idx = 0;
+
+} // end of rom_init
+
+void rom_iter(t_rom *ptr)
+{
+   uint8_t *pMem = ptr->m_mem;
+
+   uint8_t k = pMem[ptr->m_idx];
+
+   if (k < SIZE)
+   {
+      pMem[k] += 1;
+   }
+   else
+   {
+      pMem[ptr->m_idx] = 0;
+   }
+
+   ptr->m_idx += 1;
+   if (ptr->m_idx >= SIZE)
+      ptr->m_idx = 0;
+
+} // end of rom_iter
+
+void main()
+{
+   t_rom rom;
+   uint8_t i;
+
+   rom_init(&rom);
+   for (i=0; i<10; ++i)
+   {
+      rom_iter(&rom);
+   }
+
+   // Infinite loop
+   while(1)
+   {}
+
+} // end of main
+
