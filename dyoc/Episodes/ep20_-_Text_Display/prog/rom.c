101306b0c032782c9c2ba14f286343a88e2df0f4
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-05-31 22:44:34 +0200

    Minor edit.

--- dyoc/Episodes/ep20_-_Text_Display/prog/rom.c
+++ dyoc/Episodes/ep20_-_Text_Display/prog/rom.c
@@ -5,11 +5,11 @@ void main()
    uint16_t val = 0;
    uint8_t i;
 
-   for (i=0; i<10; ++i)
+   for (i=0; i<20; ++i)
    {
       printfHex16(val);
       printf("\n");
-      val = (val<<1) ^ 0x4321;
+      val = ((val<<1) + (val>>15)) ^ 0x4321;
    }
 
    // Infinite loop
