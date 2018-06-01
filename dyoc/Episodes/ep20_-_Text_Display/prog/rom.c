7b9087cea040e21390ce5c46d97a7b010ef2a008
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-06-01 07:55:09 +0200

    Changed to simple hello world program.

--- dyoc/Episodes/ep20_-_Text_Display/prog/rom.c
+++ dyoc/Episodes/ep20_-_Text_Display/prog/rom.c
@@ -2,20 +2,9 @@
 
 void main()
 {
-   uint16_t val = 0;
-   uint8_t i;
-
-   for (i=0; i<20; ++i)
+   for (i=0; i<10; ++i)
    {
-      printfHex16(val);
-      printf("\n");
-      val = ((val<<1) + (val>>15)) ^ 0x4321;
+      printf("Hello World!\n");
    }
-
-   // Infinite loop
-   while(1)
-   {
-   }
-
 } // end of main
 
