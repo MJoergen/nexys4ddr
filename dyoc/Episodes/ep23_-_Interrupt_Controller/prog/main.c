27eed773daf20281f4cfce078dffe8b56a0a290c
Michael Jørgensen <mfj@silicom.dk>
2018-06-07 15:36:32 +0200

    Minor edits.

--- dyoc/Episodes/ep23_-_Interrupt_Controller/prog/main.c
+++ dyoc/Episodes/ep23_-_Interrupt_Controller/prog/main.c
@@ -2,14 +2,14 @@
 #include "printf.h"
 #include "memorymap.h"
 
-// Called from crt0.s
 void main()
 {
    *VGA_PIX_Y_INT = 1;        // Let VGA generate interrupt at end of line 1.
    *IRQ_MASK = 1;             // Enable VGA IRQ Y interupt.
    CLI();                     // Enable CPU interrupts.
 
-   // Just go into a busy loop
+   // Just go into a busy loop.
+   // All the processing happens in lib/vga_isr.s
    while (1)
    {
    }
