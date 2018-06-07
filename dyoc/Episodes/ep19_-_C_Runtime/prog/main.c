c727eb68487009b703f8b96e6370844f491368fb
Michael Jørgensen <mfj@silicom.dk>
2018-06-07 14:22:28 +0200

    Cleanup.

--- dyoc/Episodes/ep19_-_C_Runtime/prog/main.c
+++ dyoc/Episodes/ep19_-_C_Runtime/prog/main.c
@@ -1,54 +1,53 @@
 // A little program to do some array manipulations. This is just a simple test of the toolchain and the CPU implementation.
-// Call first the rom_init() function to initialize.
-// After 100 calls to rom_iter() the m_mem[] array should contain the following:
+// Call first the init() function to initialize.
+// After 100 calls to iter() the mem[] array should contain the following:
 // 1 7 7 2 1 2 1 2 0 2
-// and the m_idx variable should contain zero.
+// and the idx variable should contain zero.
 
 #include <stdint.h>
-#include "main.h"
 
-void rom_init(t_rom *ptr)
+#define SIZE 10
+
+uint8_t mem[SIZE];
+uint8_t idx;
+
+
+void iter()
 {
-   uint8_t i;
-
-   for (i=0; i<SIZE; ++i)
-      ptr->m_mem[i] = 0;
-
-   ptr->m_idx = 0;
-
-} // end of rom_init
-
-void rom_iter(t_rom *ptr)
-{
-   uint8_t *pMem = ptr->m_mem;
-
-   uint8_t k = pMem[ptr->m_idx];
+   uint8_t k = mem[idx];
 
    if (k < SIZE)
    {
-      pMem[k] += 1;
+      mem[k] += 1;
    }
    else
    {
-      pMem[ptr->m_idx] = 0;
+      mem[idx] = 0;
    }
 
-   ptr->m_idx += 1;
-   if (ptr->m_idx >= SIZE)
-      ptr->m_idx = 0;
+   idx += 1;
+   if (idx >= SIZE)
+      idx = 0;
 
-} // end of rom_iter
+} // end of iter
+
 
 void main()
 {
-   t_rom rom;
    uint8_t i;
 
-   rom_init(&rom);
-   for (i=0; i<10; ++i)
+   // Initialize
+   for (i=0; i<SIZE; ++i)
+      mem[i] = 0;
+   idx = 0;
+
+
+   // Do the calculations
+   for (i=0; i<100; ++i)
    {
-      rom_iter(&rom);
+      iter();
    }
+
 
    // Infinite loop
    while(1)
