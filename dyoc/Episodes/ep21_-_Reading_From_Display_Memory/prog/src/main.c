5902284c8197fae9d78d60c0a32cd8f7390c64ca
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-06-09 16:23:54 +0200

    Now uses printf from standard library.

--- dyoc/Episodes/ep21_-_Reading_From_Display_Memory/prog/src/main.c
+++ dyoc/Episodes/ep21_-_Reading_From_Display_Memory/prog/src/main.c
@@ -1,13 +1,12 @@
-#include "printf.h"
+#include <stdint.h>
+#include <stdio.h>
 
 void main()
 {
    uint8_t i;
    for (i=0; i<70; ++i)
    {
-      printf("Hello World: ");
-      printfHex8(i);
-      printf("\n");
+      printf("Hello World: %d\n", i);
    }
 } // end of main
 
