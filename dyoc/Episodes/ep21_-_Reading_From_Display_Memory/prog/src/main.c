2399273ebfbdbe088c342cef67d4c807b625e6d2
Michael Jørgensen <mfj@silicom.dk>
2018-06-11 11:48:52 +0200

    Copied from episode 20.

--- dyoc/Episodes/ep21_-_Reading_From_Display_Memory/prog/src/main.c
+++ dyoc/Episodes/ep21_-_Reading_From_Display_Memory/prog/src/main.c
@@ -2,25 +2,33 @@
 // a chess board.
 // The expected number of solutions is 92.
 // See https://en.wikipedia.org/wiki/Eight_queens_puzzle
+//
+// It prints out all the solutions.
 
+#include <stdint.h>
 #include <stdio.h>
 
 #define SIZE 8
 
-static void calculate_valid(int *valid, const int *pos)
+uint8_t  pos[SIZE];
+uint8_t  valid[SIZE];
+uint8_t  solutions = 0;
+uint16_t iterations = 0;
+
+static void calculate_valid(void)
 {
-   int val=1;
-   int q;
-   int c;
+   uint8_t val=1;
+   uint8_t q;
+   uint8_t c;
    for (q=1; q<SIZE; ++q)
    {
       if (val) // This is just an optimization
       {
          for (c=0; c<q; ++c)
          {
-            val = val && (pos[c] != pos[q]) &&
-               (pos[c] + (q-c) != pos[q]) &&
-               (pos[c] != pos[q] + (q-c));
+            val = val && (pos[c]         != pos[q]) &&
+                         (pos[c] + (q-c) != pos[q]) &&
+                         (pos[c]         != pos[q] + (q-c));
          }
       }
       valid[q] = val;
@@ -28,24 +36,20 @@ static void calculate_valid(int *valid, const int *pos)
 
 } // end of calculate_valid
 
-static void print_all(const int *p)
+static void print_all(void)
 {
-   int q;
+   uint8_t q;
 
    for (q=0; q<SIZE; ++q)
    {
-      printf("%d ", p[q]);
+      printf("%d ", pos[q]);
    }
    printf("\n");
 } // end of print_all
 
 int main()
 {
-   int pos[SIZE];
-   int valid[SIZE];
-   int q;
-   int solutions = 0;
-   int iterations = 0;
+   int8_t q;   // Must be a signed type
 
    for (q=0; q<SIZE; ++q)
    {
@@ -57,16 +61,16 @@ int main()
    {
       iterations++;
 
-      calculate_valid(valid, pos);
+      calculate_valid();
       if (valid[SIZE-1])
       {
          solutions++;
-         print_all(pos);
+         print_all();
       }
 
       for (q=SIZE-1; q>=0; --q)
       {
-         int val = (q==0) || (valid[q-1]);
+         uint8_t val = (q==0) || (valid[q-1]);
          if (val && (pos[q]<SIZE-1))
          {
             pos[q]++;
@@ -79,7 +83,6 @@ int main()
       {
          break; // out of while loop
       }
-
    } // end of while (1)
 
    printf("%d iterations.\n", iterations);
