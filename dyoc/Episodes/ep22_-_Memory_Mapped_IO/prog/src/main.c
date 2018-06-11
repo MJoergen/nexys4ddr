c7049c2aa26965eaf17e972ad200686cba4dd539
Michael Jørgensen <mfj@silicom.dk>
2018-06-11 12:01:42 +0200

    Copied from episode 21 and added coloured background.

--- dyoc/Episodes/ep22_-_Memory_Mapped_IO/prog/src/main.c
+++ dyoc/Episodes/ep22_-_Memory_Mapped_IO/prog/src/main.c
@@ -3,31 +3,36 @@
 // The expected number of solutions is 92.
 // See https://en.wikipedia.org/wiki/Eight_queens_puzzle
 //
+// It prints out all the solutions.
+//
 // At the same time, it attempts to maintain a visual pattern synchronous with the
 // VGA output.
 
+#include <stdint.h>
 #include <stdio.h>
 #include "memorymap.h"
 
-#define H_PIXELS 640
-#define V_PIXELS 480
-
 #define SIZE 8
 
-static void calculate_valid(int *valid, const int *pos)
+uint8_t  pos[SIZE];
+uint8_t  valid[SIZE];
+uint8_t  solutions = 0;
+uint16_t iterations = 0;
+
+static void calculate_valid(void)
 {
-   int val=1;
-   int q;
-   int c;
+   uint8_t val=1;
+   uint8_t q;
+   uint8_t c;
    for (q=1; q<SIZE; ++q)
    {
       if (val) // This is just an optimization
       {
          for (c=0; c<q; ++c)
          {
-            val = val && (pos[c] != pos[q]) &&
-               (pos[c] + (q-c) != pos[q]) &&
-               (pos[c] != pos[q] + (q-c));
+            val = val && (pos[c]         != pos[q]) &&
+                         (pos[c] + (q-c) != pos[q]) &&
+                         (pos[c]         != pos[q] + (q-c));
          }
       }
       valid[q] = val;
@@ -38,13 +43,13 @@ static void calculate_valid(int *valid, const int *pos)
 
 } // end of calculate_valid
 
-static void print_all(const int *p)
+static void print_all(void)
 {
-   int q;
+   uint8_t q;
 
    for (q=0; q<SIZE; ++q)
    {
-      printf("%d ", p[q]);
+      printf("%d ", pos[q]);
 
       // Set background colour equal to the current line number.
       VGA_PALETTE[0] = *VGA_PIX_Y;
@@ -54,11 +59,7 @@ static void print_all(const int *p)
 
 int main()
 {
-   int pos[SIZE];
-   int valid[SIZE];
-   int q;
-   int solutions = 0;
-   int iterations = 0;
+   int8_t q;   // Must be a signed type
 
    for (q=0; q<SIZE; ++q)
    {
@@ -70,16 +71,16 @@ int main()
    {
       iterations++;
 
-      calculate_valid(valid, pos);
+      calculate_valid();
       if (valid[SIZE-1])
       {
          solutions++;
-         print_all(pos);
+         print_all();
       }
 
       for (q=SIZE-1; q>=0; --q)
       {
-         int val = (q==0) || (valid[q-1]);
+         uint8_t val = (q==0) || (valid[q-1]);
          if (val && (pos[q]<SIZE-1))
          {
             pos[q]++;
