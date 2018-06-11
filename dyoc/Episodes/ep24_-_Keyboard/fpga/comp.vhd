163189e10a72cabed99bb57165684c5450956659
Michael Jørgensen <mfj@silicom.dk>
2018-06-11 13:45:06 +0200

    Synthesis error fixed.

--- dyoc/Episodes/ep24_-_Keyboard/fpga/comp.vhd
+++ dyoc/Episodes/ep24_-_Keyboard/fpga/comp.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
 -- This is the top level module. The ports on this entity are mapped directly
 -- to pins on the FPGA.
