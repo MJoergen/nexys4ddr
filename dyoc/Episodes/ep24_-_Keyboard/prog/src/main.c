696f2b769ee1298d0f75e2099a965077d85c8740
Michael Jørgensen <mfj@silicom.dk>
2018-06-11 14:59:42 +0200

    Cleanup.

--- dyoc/Episodes/ep24_-_Keyboard/prog/src/main.c
+++ dyoc/Episodes/ep24_-_Keyboard/prog/src/main.c
@@ -1,27 +1,16 @@
-#include <6502.h>                // CLI()
 #include <stdint.h>
 #include <stdio.h>
-#include "keyboard.h"
-#include "memorymap.h"           // IRQ_KBD_NUM
+#include "keyboard.h"            // kbd_buffer_pop()
+#include "memorymap.h"           // CPU_CYC
 
 void main()
 {
-   uint8_t dummy;
-
-   // Do a small timing measurement to begin with
-   uint32_t t1 = *CPU_CYC;
-   uint32_t t2 = *CPU_CYC;
-   printf("%04x\n", t2-t1);
-
-   dummy = *IRQ_STATUS;             // Clear any pending interrupts.
-   *IRQ_MASK = 1<<IRQ_KBD_NUM;      // Enable keyboard interrupts.
-   CLI();                           // Enable CPU interrupts.
-
    // Just go into a busy loop.
    while (1)
    {
       uint8_t ev = kbd_buffer_pop();   // This does a BLOCKING wait.
-      printf("%02x\n", ev);
+      uint16_t t = *(uint16_t *)CPU_CYC;
+      printf("%04x %02x\n", t, ev);
    }
 
 } // end of main
