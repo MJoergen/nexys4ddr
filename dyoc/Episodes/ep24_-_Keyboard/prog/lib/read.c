a131c1b0ef5bd2cb0d235009a93280801bb5ebc3
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-06-13 22:54:38 +0200

    Cleanup.

--- dyoc/Episodes/ep24_-_Keyboard/prog/lib/read.c
+++ dyoc/Episodes/ep24_-_Keyboard/prog/lib/read.c
@@ -1,8 +1,6 @@
 #include <stdint.h>     // uint8_t, etc.
 #include <conio.h>      // cgetc()
 
-uint8_t getch();
-
 // This is just a very simple implementation of the read() function.
 
 // For now, we just ignore the file descriptor fd.
