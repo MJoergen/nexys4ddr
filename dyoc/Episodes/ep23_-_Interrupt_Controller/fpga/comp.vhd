a2358ac5385ec14d1168f90409ea5d9d42e77613
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-09-23 19:56:17 +0200

    Cleanup.

--- dyoc/Episodes/ep23_-_Interrupt_Controller/fpga/comp.vhd
+++ dyoc/Episodes/ep23_-_Interrupt_Controller/fpga/comp.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
-use ieee.numeric_std.all;
 
 -- This is the top level module. The ports on this entity are mapped directly
 -- to pins on the FPGA.
