aaae698c90c9439b00454deb2d2620f034d8f8d1
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-09-23 13:42:43 +0200

    Fixed synthesis error.

--- dyoc/Episodes/ep22_-_Memory_Mapped_IO/fpga/comp.tcl
+++ dyoc/Episodes/ep22_-_Memory_Mapped_IO/fpga/comp.tcl
@@ -2,7 +2,7 @@
 read_vhdl { \
    vga/overlay.vhd vga/chars.vhd vga/font.vhd vga/vga.vhd \
    mem/memio.vhd mem/dmem.vhd mem/ram.vhd mem/rom.vhd mem/mem.vhd \
-   cpu/datapath.vhd cpu/ctl.vhd cpu/cpu.vhd cpu/alu.vhd \
+   cpu/datapath.vhd cpu/ctl.vhd cpu/cpu.vhd cpu/alu.vhd cpu/cycle.vhd \
    comp.vhd}
 read_xdc comp.xdc
 synth_design -top comp -part xc7a100tcsg324-1 -flatten_hierarchy none
