44eb99a4ed37a9b1d95e17ac68f8f3ca60bcc844
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-10-10 09:13:58 +0200

    Removed CPU clock cycle counter.

--- dyoc/Episodes/ep24_-_Keyboard/fpga/comp.tcl
+++ dyoc/Episodes/ep24_-_Keyboard/fpga/comp.tcl
@@ -2,9 +2,9 @@
 read_vhdl { \
    chipset/ic.vhd chipset/waiter.vhd chipset/timer.vhd \
    vga/overlay.vhd vga/chars.vhd vga/font.vhd vga/vga.vhd \
-   mem/memio.vhd mem/dmem.vhd mem/ram.vhd mem/rom.vhd mem/mem.vhd \
+   mem/dmem.vhd mem/ram.vhd mem/rom.vhd mem/mem.vhd mem/memio.vhd \
    keyboard/ps2.vhd keyboard/scancode.vhd keyboard/keyboard.vhd \
-   cpu/zp.vhd cpu/sr.vhd cpu/regfile.vhd cpu/hilo.vhd cpu/pc.vhd cpu/cycle.vhd cpu/datapath.vhd cpu/ctl.vhd cpu/cpu.vhd cpu/alu.vhd \
+   cpu/zp.vhd cpu/sr.vhd cpu/regfile.vhd cpu/hilo.vhd cpu/pc.vhd cpu/datapath.vhd cpu/ctl.vhd cpu/cpu.vhd cpu/alu.vhd \
    comp.vhd}
 read_xdc comp.xdc
 synth_design -top comp -part xc7a100tcsg324-1 -flatten_hierarchy none
