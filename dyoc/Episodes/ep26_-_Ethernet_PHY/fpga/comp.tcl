e800d45b1cf62a951ef1fc771d0dff232bf7ada4
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-10-10 18:35:48 +0200

    Cleanup.

--- dyoc/Episodes/ep26_-_Ethernet_PHY/fpga/comp.tcl
+++ dyoc/Episodes/ep26_-_Ethernet_PHY/fpga/comp.tcl
@@ -2,9 +2,9 @@
 read_vhdl { \
    chipset/ic.vhd chipset/waiter.vhd chipset/timer.vhd \
    vga/overlay.vhd vga/chars.vhd vga/font.vhd vga/vga.vhd \
-   mem/memio.vhd mem/dmem.vhd mem/ram.vhd mem/rom.vhd mem/mem.vhd \
+   mem/dmem.vhd mem/ram.vhd mem/rom.vhd mem/mem.vhd mem/memio.vhd \
    keyboard/ps2.vhd keyboard/scancode.vhd keyboard/keyboard.vhd \
-   cpu/zp.vhd cpu/sr.vhd cpu/regfile.vhd cpu/hilo.vhd cpu/pc.vhd cpu/cycle.vhd cpu/datapath.vhd cpu/ctl.vhd cpu/cpu.vhd cpu/alu.vhd \
+   cpu/zp.vhd cpu/sr.vhd cpu/regfile.vhd cpu/hilo.vhd cpu/pc.vhd cpu/datapath.vhd cpu/ctl.vhd cpu/cpu.vhd cpu/alu.vhd cpu/cycle.vhd \
    ethernet/ethernet.vhd ethernet/lan8720a/lan8720a.vhd ethernet/lan8720a/rmii_rx.vhd ethernet/lan8720a/rmii_tx.vhd \
    comp.vhd}
 read_xdc comp.xdc
