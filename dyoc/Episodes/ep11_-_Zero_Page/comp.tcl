715a0317aab1c9cef23cd6f8285e32f41d0cd199
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-10-29 06:56:46 +0100

    Now uses separate clock domains.

--- dyoc/Episodes/ep11_-_Zero_Page/comp.tcl
+++ dyoc/Episodes/ep11_-_Zero_Page/comp.tcl
@@ -1,9 +1,12 @@
 # This is a tcl command script for the Vivado tool chain
-read_vhdl {comp.vhd \
+read_vhdl { \
+   comp.vhd clk.vhd waiter.vhd main.vhd \
    vga/font.vhd vga/digits.vhd vga/vga.vhd \
-   mem/ram.vhd mem/rom.vhd mem/mem.vhd \
-   cpu/datapath.vhd cpu/ctl.vhd cpu/cpu.vhd cpu/alu.vhd}
+   cpu/datapath.vhd cpu/ctl.vhd cpu/cpu.vhd cpu/alu.vhd \
+   mem/mem.vhd mem/rom.vhd mem/ram.vhd
+}
 read_xdc comp.xdc
+set_property XPM_LIBRARIES {XPM_CDC} [current_project]
 synth_design -top comp -part xc7a100tcsg324-1 -flatten_hierarchy none
 place_design
 route_design
