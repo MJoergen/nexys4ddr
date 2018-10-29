f44959157d528445d79251cf8728c896b3a17b98
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-10-29 06:28:44 +0100

    Testbench now instantiates MAIN.

---
+++ dyoc/Episodes/ep09_-_Branching/main_tb.vhd
@@ -0,0 +1,61 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity main_tb is
+end main_tb;
+
+architecture Structural of main_tb is
+
+   -- Clock
+   signal main_clk  : std_logic;
+
+   -- Generate pause signal
+   signal main_wait_cnt : std_logic_vector(1 downto 0) := (others => '0');
+   signal main_wait     : std_logic;
+
+begin
+   
+   --------------------------------------------------
+   -- Generate clock
+   --------------------------------------------------
+
+   -- Generate clock
+   main_clk_proc : process
+   begin
+      main_clk <= '1', '0' after 5 ns; -- 100 MHz
+      wait for 10 ns;
+   end process main_clk_proc;
+
+
+   --------------------------------------------------
+   -- Generate wait signal
+   --------------------------------------------------
+
+   main_wait_cnt_proc : process (main_clk)
+   begin
+      if rising_edge(main_clk) then
+         main_wait_cnt <= main_wait_cnt + 1;
+      end if;
+   end process main_wait_cnt_proc;
+
+   -- Check for wrap around of counter.
+   main_wait <= '0' when main_wait_cnt = 0  else '1';
+
+   
+   --------------------------------------------------
+   -- Instantiate MAIN
+   --------------------------------------------------
+
+   main_inst : entity work.main
+   generic map (
+      G_OVERLAY_BITS => 144
+   )
+   port map (
+      clk_i     => main_clk,
+      wait_i    => main_wait,
+      overlay_o => open
+   ); -- main_inst
+   
+end architecture Structural;
+
--- dyoc/Episodes/ep09_-_Branching/vga/digits.vhd
+++ dyoc/Episodes/ep09_-_Branching/vga/digits.vhd
@@ -17,9 +17,10 @@ entity digits is
    port (
       clk_i     : in  std_logic;
 
+      digits_i  : in  std_logic_vector(G_OVERLAY_BITS-1 downto 0);
+
       pix_x_i   : in  std_logic_vector(9 downto 0);
       pix_y_i   : in  std_logic_vector(9 downto 0);
-      digits_i  : in  std_logic_vector(G_OVERLAY_BITS-1 downto 0);
 
       vga_hs_o  : out std_logic;
       vga_vs_o  : out std_logic;
