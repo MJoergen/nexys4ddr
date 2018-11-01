8a75b7dfb3afabb4df2c93634ee8f1f84abcbc30
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-11-01 07:44:50 +0100

    Added missing files.

---
+++ dyoc/Episodes/ep19_-_C_Runtime/fpga/main_tb.vhd
@@ -0,0 +1,78 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity main_tb is
+end main_tb;
+
+architecture Structural of main_tb is
+
+   -- Clock and reset
+   signal main_clk : std_logic;
+   signal main_rst : std_logic;
+
+   -- Generate pause signal
+   signal main_wait_cnt : std_logic_vector(1 downto 0) := (others => '0');
+   signal main_wait     : std_logic;
+
+begin
+   
+   --------------------------------------------------
+   -- Generate clock
+   --------------------------------------------------
+
+   main_clk_proc : process
+   begin
+      main_clk <= '1', '0' after 5 ns; -- 100 MHz
+      wait for 10 ns;
+   end process main_clk_proc;
+
+
+   --------------------------------------------------
+   -- Generate reset
+   --------------------------------------------------
+
+   main_rst_proc : process
+   begin
+      main_rst <= '1';
+      wait for 100 ns;
+
+      -- Make sure reset is deasserted synchronuous to the clock
+      wait until main_clk = '1';
+      main_rst <= '0';
+      wait;
+   end process main_rst_proc;
+
+
+   --------------------------------------------------
+   -- Generate wait signal
+   --------------------------------------------------
+
+   main_wait_cnt_proc : process (main_clk)
+   begin
+      if rising_edge(main_clk) then
+         main_wait_cnt <= main_wait_cnt + 1;
+      end if;
+   end process main_wait_cnt_proc;
+
+   -- Check for wrap around of counter.
+   main_wait <= '0' when main_wait_cnt = 0  else '1';
+
+   
+   --------------------------------------------------
+   -- Instantiate MAIN
+   --------------------------------------------------
+
+   main_inst : entity work.main
+   generic map (
+      G_OVERLAY_BITS => 176
+   )
+   port map (
+      clk_i     => main_clk,
+      rst_i     => main_rst,
+      wait_i    => main_wait,
+      overlay_o => open
+   ); -- main_inst
+   
+end architecture Structural;
+
