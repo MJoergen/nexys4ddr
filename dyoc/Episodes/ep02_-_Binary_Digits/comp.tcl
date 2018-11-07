bad59b51944861a834daa9ba1e572ab2a4c13c04
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-11-07 07:26:43 +0100

    Moved pixel generation to separate file.

--- dyoc/Episodes/ep02_-_Binary_Digits/comp.tcl
+++ dyoc/Episodes/ep02_-_Binary_Digits/comp.tcl
@@ -1,6 +1,6 @@
 # This is a tcl command script for the Vivado tool chain
 read_vhdl {comp.vhd \
-   vga/vga.vhd vga/digits.vhd}
+   vga/vga.vhd vga/digits.vhd vga/pix.vhd}
 read_xdc comp.xdc
 synth_design -top comp -part xc7a100tcsg324-1 -flatten_hierarchy none
 place_design
