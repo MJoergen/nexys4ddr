eb0895441638c2d7dba145ba12ad8860cb77916a
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-11-12 17:41:18 +0100

    Updated to VHDL 2008.

--- dyoc/Episodes/ep14_-_Registers_X_and_Y/mem/rom.vhd
+++ dyoc/Episodes/ep14_-_Registers_X_and_Y/mem/rom.vhd
@@ -40,10 +40,11 @@ architecture Structural of rom is
 
    -- This reads the ROM contents from a text file
    impure function InitRamFromFile(RamFileName : in string) return mem_t is
-      FILE RamFile : text is in RamFileName;
+      FILE RamFile : text;
       variable RamFileLine : line;
       variable RAM : mem_t := (others => (others => '0'));
    begin
+      file_open(RamFile, RamFileName, read_mode);
       for i in mem_t'range loop
          readline (RamFile, RamFileLine);
          hread (RamFileLine, RAM(i));
