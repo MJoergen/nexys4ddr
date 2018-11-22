a20a913a21759d24b98a83c2dbe0cbc1557ae34e
Michael Jørgensen <mfj@silicom.dk>
2018-11-22 09:39:38 +0100

    Moved MAIN module to separate directory.

--- dyoc/Episodes/ep25_-_Sample_Programs/fpga/comp.tcl
+++ dyoc/Episodes/ep25_-_Sample_Programs/fpga/comp.tcl
@@ -2,9 +2,9 @@
 read_vhdl -vhdl2008 { \
    chipset/ic.vhd chipset/waiter.vhd chipset/timer.vhd \
    vga/overlay.vhd vga/chars.vhd vga/font.vhd vga/vga.vhd \
-   mem/dmem.vhd mem/ram.vhd mem/rom.vhd mem/mem.vhd mem/memio.vhd \
    keyboard/ps2.vhd keyboard/scancode.vhd keyboard/keyboard.vhd \
-   cpu/zp.vhd cpu/sr.vhd cpu/regfile.vhd cpu/hilo.vhd cpu/pc.vhd cpu/datapath.vhd cpu/ctl.vhd cpu/cpu.vhd cpu/alu.vhd cpu/cycle.vhd \
+   main/mem/dmem.vhd main/mem/ram.vhd main/mem/rom.vhd main/mem/mem.vhd main/mem/memio.vhd \
+   main/cpu/zp.vhd main/cpu/sr.vhd main/cpu/regfile.vhd main/cpu/hilo.vhd main/cpu/pc.vhd main/cpu/datapath.vhd main/cpu/ctl.vhd main/cpu/cpu.vhd main/cpu/alu.vhd main/cpu/cycle.vhd \
    comp.vhd}
 read_xdc comp.xdc
 synth_design -top comp -part xc7a100tcsg324-1 -flatten_hierarchy none
