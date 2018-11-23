475c5a76232befd6b2681b6b0d00cdafbb7fb0d5
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-11-23 07:11:33 +0100

    Added separate clock domains. Moved CPU and MEM to a new MAIN module. Now displays assembly mnemonic on overlay.

---
+++ dyoc/Episodes/ep23_-_Interrupt_Controller/fpga/main/cpu/ar.vhd
@@ -0,0 +1,39 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std_unsigned.all;
+
+entity ar is
+   port (
+      clk_i    : in  std_logic;
+      wait_i   : in  std_logic;
+      ar_sel_i : in  std_logic;
+      alu_ar_i : in  std_logic_vector(7 downto 0);
+
+      ar_o     : out std_logic_vector(7 downto 0)
+   );
+end entity ar;
+
+architecture structural of ar is
+
+   -- 'A' register
+   signal ar : std_logic_vector(7 downto 0);
+
+begin
+
+   -- 'A' register
+   ar_proc : process (clk_i)
+   begin
+      if rising_edge(clk_i) then
+         if wait_i = '0' then
+            if ar_sel_i = '1' then
+               ar <= alu_ar_i;
+            end if;
+         end if;
+      end if;
+   end process ar_proc;
+
+   -- Drive output signal
+   ar_o <= ar;
+
+end architecture structural;
+
