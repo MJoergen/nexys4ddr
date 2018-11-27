23d26954cf711b7118f0f8bbc60453e90bad20d0
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-11-27 06:59:39 +0100

    Removed unneeded 'use' clause.

--- dyoc/Episodes/ep24_-_Keyboard/fpga/comp.tcl
+++ dyoc/Episodes/ep24_-_Keyboard/fpga/comp.tcl
@@ -1,17 +1,20 @@
 # This is a tcl command script for the Vivado tool chain
 read_vhdl -vhdl2008 { \
-   chipset/ic.vhd chipset/waiter.vhd chipset/timer.vhd \
-   vga/overlay.vhd vga/chars.vhd vga/font.vhd vga/vga.vhd \
+   comp.vhd chipset/waiter.vhd chipset/clk.vhd chipset/cdc.vhd chipset/ic.vhd chipset/timer.vhd \
+   vga/vga.vhd vga/overlay.vhd vga/pix.vhd vga/font.vhd vga/chars.vhd vga/opcodes.vhd \
    keyboard/ps2.vhd keyboard/scancode.vhd keyboard/keyboard.vhd \
-   main/mem/dmem.vhd main/mem/ram.vhd main/mem/rom.vhd main/mem/mem.vhd main/mem/memio.vhd \
-   main/cpu/zp.vhd main/cpu/sr.vhd main/cpu/regfile.vhd main/cpu/hilo.vhd main/cpu/pc.vhd main/cpu/datapath.vhd main/cpu/ctl.vhd main/cpu/cpu.vhd main/cpu/alu.vhd \
-   comp.vhd}
+   main/main.vhd \
+   main/cpu/cpu.vhd main/cpu/datapath.vhd main/cpu/ctl.vhd main/cpu/pc.vhd main/cpu/ar.vhd main/cpu/hi.vhd main/cpu/lo.vhd main/cpu/alu.vhd main/cpu/sr.vhd main/cpu/sp.vhd main/cpu/xr.vhd main/cpu/yr.vhd main/cpu/zp.vhd \
+   main/mem/mem.vhd main/mem/rom.vhd main/mem/ram.vhd main/mem/dmem.vhd main/mem/memio.vhd \
+}
 read_xdc comp.xdc
+set_property XPM_LIBRARIES {XPM_FIFO} [current_project]
 synth_design -top comp -part xc7a100tcsg324-1 -flatten_hierarchy none
 place_design
 route_design
 write_checkpoint -force comp.dcp
 write_bitstream -force comp.bit
-
+report_methodology
+report_timing_summary -file timing_summary.rpt
 source bmm.tcl
 exit
--- dyoc/Episodes/ep24_-_Keyboard/fpga/comp.vhd
+++ dyoc/Episodes/ep24_-_Keyboard/fpga/comp.vhd
@@ -1,12 +1,12 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std_unsigned.all;
 
 -- This is the top level module. The ports on this entity are mapped directly
 -- to pins on the FPGA.
 --
 -- In this version the design can execute all instructions.
--- It additionally features a 80x60 character display.
+-- It additionally features a 80x60 character display and
+-- a keyboard interface.
 --
 -- The speed of the execution is controlled by the slide switches.
 -- Simultaneously, the CPU debug is shown as an overlay over the text screen.
@@ -14,27 +14,16 @@ use ieee.numeric_std_unsigned.all;
 -- CPU debug overlay is switched off.
 
 entity comp is
-   generic (
-      G_SIM_MODEL : boolean := false;  -- This is set to true in the simulation test bench.
-      G_FONT_FILE : string := "font8x8.txt"
-   );
    port (
       clk_i      : in  std_logic;                      -- 100 MHz
 
-      -- CPU reset (push button). Active low.
+      sw_i       : in  std_logic_vector(7 downto 0);
+      led_o      : out std_logic_vector(7 downto 0);
       rstn_i     : in  std_logic;
 
-      -- Input switches
-      sw_i       : in  std_logic_vector(7 downto 0);
-
-      -- Output LED's
-      led_o      : out std_logic_vector(7 downto 0);
-
-      -- Keyboard
       ps2_clk_i  : in  std_logic;
       ps2_data_i : in  std_logic;
 
-      -- Output to VGA monitor
       vga_hs_o   : out std_logic;
       vga_vs_o   : out std_logic;
       vga_col_o  : out std_logic_vector(7 downto 0)    -- RRRGGGBB
@@ -43,119 +32,81 @@ end comp;
 
 architecture structural of comp is
 
-   -- Clock divider for VGA
-   signal clk_cnt  : std_logic_vector(1 downto 0) := (others => '0');
-   signal vga_clk  : std_logic;
+   constant C_OVERLAY_BITS  : integer := 176;
+   constant C_ROM_INIT_FILE : string := "../rom.txt";
+   constant C_OPCODES_FILE  : string := "opcodes.txt";
+   constant C_FONT_FILE     : string := "font8x8.txt";
 
-   -- Reset
-   signal rst : std_logic := '1';   -- Make sure reset is asserted after power-up.
+   -- MAIN Clock domain
+   signal main_clk            : std_logic;
+   signal main_rst            : std_logic;
+   signal main_rst_shr        : std_logic_vector(7 downto 0) := X"FF";
+   signal main_wait           : std_logic;
+   signal main_overlay        : std_logic_vector(C_OVERLAY_BITS-1 downto 0);
+   signal main_memio_wr       : std_logic_vector(8*32-1 downto 0);
+   signal main_memio_rd       : std_logic_vector(8*32-1 downto 0);
 
-   -- Wait signal is used to slow down the CPU
-   signal sys_wait      : std_logic;
-
-   -- VGA debug overlay
-   signal vga_overlay_en : std_logic;
-   signal vga_overlay    : std_logic_vector(191 downto 0);
-
-   -- Data Path signals
-   signal cpu_addr  : std_logic_vector(15 downto 0);
-   signal mem_data  : std_logic_vector( 7 downto 0);
-   signal cpu_data  : std_logic_vector( 7 downto 0);
-   signal cpu_rden  : std_logic;
-   signal cpu_wren  : std_logic;
-   signal cpu_debug : std_logic_vector(175 downto 0);
-   signal cpu_wait  : std_logic;
-   signal mem_wait  : std_logic;
-
-   -- Interface between VGA and Memory
-   signal char_addr : std_logic_vector(12 downto 0);
-   signal char_data : std_logic_vector( 7 downto 0);
-   signal col_addr  : std_logic_vector(12 downto 0);
-   signal col_data  : std_logic_vector( 7 downto 0);
-
-   -- Memory Mapped I/O
-   signal memio_rd   : std_logic_vector(8*32-1 downto 0);
-   signal memio_rden : std_logic_vector(  32-1 downto 0);
-   signal memio_wr   : std_logic_vector(8*32-1 downto 0);
-
-   signal vga_memio_palette   : std_logic_vector(16*8-1 downto 0);
+   -- VGA Clock doamin
+   signal vga_clk             : std_logic;
+   signal vga_rst             : std_logic;
+   signal vga_rst_shr         : std_logic_vector(7 downto 0) := X"FF";
+   signal vga_overlay_en      : std_logic;
+   signal vga_overlay         : std_logic_vector(C_OVERLAY_BITS-1 downto 0);
+   signal vga_char_addr       : std_logic_vector( 12 downto 0);
+   signal vga_char_data       : std_logic_vector(  7 downto 0);
+   signal vga_col_addr        : std_logic_vector( 12 downto 0);
+   signal vga_col_data        : std_logic_vector(  7 downto 0);
+   signal vga_memio_palette   : std_logic_vector(127 downto 0);
    signal vga_memio_pix_y_int : std_logic_vector( 2*8-1 downto 0);
-   signal vga_memio_pix_x     : std_logic_vector( 2*8-1 downto 0);
-   signal vga_memio_pix_y     : std_logic_vector( 2*8-1 downto 0);
-
-   signal kbd_memio_data : std_logic_vector( 1*8-1 downto 0);
-
-   signal irq_memio_mask   : std_logic_vector( 1*8-1 downto 0);
-   signal irq_memio_status : std_logic_vector( 1*8-1 downto 0);
-   signal irq_memio_clear  : std_logic;
-
-   -- Interrupt controller
-   signal ic_irq    : std_logic_vector(7 downto 0);
-   signal cpu_irq   : std_logic;
-   signal vga_irq   : std_logic;
-   signal kbd_irq   : std_logic;
-   signal timer_irq : std_logic := '0';
-
-   signal kbd_debug : std_logic_vector(15 downto 0);
-
+   signal vga_memio_pix_x     : std_logic_vector( 15 downto 0);
+   signal vga_memio_pix_y     : std_logic_vector( 15 downto 0);
+   signal vga_irq             : std_logic;
+   
 begin
 
    --------------------------------------------------
-   -- Divide input clock by 4, from 100 MHz to 25 MHz
-   -- This is close enough to 25.175 MHz needed by VGA.
+   -- Instantiate Clock generation
    --------------------------------------------------
 
-   p_clk_cnt : process (clk_i)
-   begin
-      if rising_edge(clk_i) then
-         clk_cnt <= clk_cnt + 1;
-      end if;
-   end process p_clk_cnt;
-
-   vga_clk <= clk_cnt(1);
+   clk_inst : entity work.clk_wiz_0_clk_wiz
+   port map (
+      clk_in1  => clk_i,
+      eth_clk  => open, -- Not needed yet.
+      vga_clk  => vga_clk,
+      main_clk => main_clk
+   ); -- clk_inst
 
 
    --------------------------------------------------
    -- Generate Reset
    --------------------------------------------------
 
-   p_rst : process (vga_clk)
+   main_rst_proc : process (main_clk)
+   begin
+      if rising_edge(main_clk) then
+         -- Hold reset asserted for a number of clock cycles.
+         main_rst     <= main_rst_shr(0);
+         main_rst_shr <= "0" & main_rst_shr(main_rst_shr'left downto 1);
+
+         if rstn_i = '0' then
+            main_rst_shr <= (others => '1');
+         end if;
+      end if;
+   end process main_rst_proc;
+
+   vga_rst_proc : process (vga_clk)
    begin
       if rising_edge(vga_clk) then
-         rst <= not rstn_i;
+         -- Hold reset asserted for a number of clock cycles.
+         vga_rst     <= vga_rst_shr(0);
+         vga_rst_shr <= "0" & vga_rst_shr(vga_rst_shr'left downto 1);
+
+         if rstn_i = '0' then
+            vga_rst_shr <= (others => '1');
+         end if;
       end if;
-   end process p_rst;
-
-
-   --------------------------------------------------
-   -- Instantiate interrupt controller
-   --------------------------------------------------
-
-   i_ic : entity work.ic
-   port map (
-      clk_i   => vga_clk,
-      irq_i   => ic_irq,    -- Eight independent interrupt sources
-      irq_o   => cpu_irq,   -- Overall CPU interrupt
-
-      mask_i     => irq_memio_mask,      -- IRQ mask
-      stat_o     => irq_memio_status,    -- IRQ status
-      stat_clr_i => irq_memio_clear      -- Reading from IRQ status
-   );
-
-
-   --------------------------------------------------
-   -- Instantiate Timer
-   --------------------------------------------------
-
-   i_timer : entity work.timer
-   generic map (
-      G_TIMER_CNT => 250000    -- Generate interrupt every 0.01 seconds
-   )
-   port map (
-      clk_i => vga_clk,
-      irq_o => timer_irq
-   );
-
+   end process vga_rst_proc;
+   
 
    --------------------------------------------------
    -- Instantiate Waiter
@@ -168,9 +119,49 @@ begin
       wait_o => sys_wait
    );
 
-   -- Generate wait signal for the CPU.
-   cpu_wait <= mem_wait or sys_wait;
 
+   --------------------------------------------------
+   -- Instantiate MAIN module
+   --------------------------------------------------
+
+   main_inst : entity work.main
+   generic map (
+      G_ROM_INIT_FILE => C_ROM_INIT_FILE,
+      G_OVERLAY_BITS  => C_OVERLAY_BITS
+   )
+   port map (
+      main_clk_i      => main_clk,
+      main_rst_i      => main_rst,
+      main_wait_i     => main_wait,
+      main_led_o      => led_o,
+      main_overlay_o  => main_overlay,
+      main_memio_wr_o => main_memio_wr,
+      main_memio_rd_i => main_memio_rd,
+      --
+      vga_clk_i       => vga_clk,
+      vga_char_addr_i => vga_char_addr,
+      vga_char_data_o => vga_char_data,
+      vga_col_addr_i  => vga_col_addr,
+      vga_col_data_o  => vga_col_data
+   ); -- main_inst
+   
+
+   --------------------------------------------------
+   -- Instantiate clock crossing from MAIN to VGA
+   --------------------------------------------------
+
+   cdc_overlay_inst : entity work.cdc
+   generic map (
+      G_WIDTH => C_OVERLAY_BITS
+   )
+   port map (
+      src_clk_i  => main_clk,
+      src_rst_i  => main_rst,
+      src_data_i => main_overlay,
+      dst_clk_i  => vga_clk,
+      dst_data_o => vga_overlay
+   ); -- cdc_overlay_inst
+   
 
    --------------------------------------------------
    -- Control VGA debug overlay
@@ -180,76 +171,14 @@ begin
 
 
    --------------------------------------------------
-   -- Instantiate CPU
-   --------------------------------------------------
-
-   i_cpu : entity work.cpu
-   port map (
-      clk_i     => vga_clk,
-      wait_i    => cpu_wait,
-      addr_o    => cpu_addr,
-      rden_o    => cpu_rden,
-      data_i    => mem_data,
-      wren_o    => cpu_wren,
-      data_o    => cpu_data,
-      invalid_o => led_o,
-      debug_o   => cpu_debug,
-      irq_i     => cpu_irq,
-      nmi_i     => '0', -- Not used at the moment
-      rst_i     => rst
-   );
-
-
-   --------------------------------------------------
-   -- Instantiate memory
-   --------------------------------------------------
-
-   i_mem : entity work.mem
-   generic map (
-      G_ROM_SIZE   => 14, -- 16 Kbytes
-      G_RAM_SIZE   => 15, -- 32 Kbytes
-      G_CHAR_SIZE  => 13, -- 8 Kbytes
-      G_COL_SIZE   => 13, -- 8 Kbytes
-      G_MEMIO_SIZE =>  6, -- 64 bytes
-      --
-      G_ROM_MASK   => X"C000",
-      G_RAM_MASK   => X"0000",
-      G_CHAR_MASK  => X"8000",
-      G_COL_MASK   => X"A000",
-      G_MEMIO_MASK => X"7FC0",
-      --
-      G_ROM_FILE   => "../rom.txt",
-      G_MEMIO_INIT => X"00000000000000000000000000000000" &
-                      X"FFFCE3E0433C1E178C82803022110A00"
-   )
-   port map (
-      clk_i    => vga_clk,
-      --
-      a_addr_i => cpu_addr,  -- Only select the relevant address bits
-      a_data_o => mem_data,
-      a_rden_i => cpu_rden,
-      a_wren_i => cpu_wren,
-      a_data_i => cpu_data,
-      a_wait_o => mem_wait,
-      --
-      b_char_addr_i => char_addr,
-      b_char_data_o => char_data,
-      b_col_addr_i  => col_addr,
-      b_col_data_o  => col_data,
-      --
-      b_memio_rd_i   => memio_rd,    -- To MEMIO
-      b_memio_rden_o => memio_rden,  -- To MEMIO
-      b_memio_wr_o   => memio_wr     -- From MEMIO
-   );
-
-
-   --------------------------------------------------
    -- Instantiate VGA module
    --------------------------------------------------
 
-   i_vga : entity work.vga
+   vga_inst : entity work.vga
    generic map (
-      G_FONT_FILE => G_FONT_FILE
+      G_OVERLAY_BITS => C_OVERLAY_BITS,
+      G_OPCODES_FILE => C_OPCODES_FILE,
+      G_FONT_FILE    => G_FONT_FILE
    )
    port map (
       clk_i     => vga_clk,
@@ -269,7 +198,7 @@ begin
       memio_pix_x_o     => vga_memio_pix_x,
       memio_pix_y_o     => vga_memio_pix_y,
       irq_o             => vga_irq
-   );
+   ); -- vga_inst
 
 
    ------------------------------
@@ -295,43 +224,62 @@ begin
    --------------------------------------------------
 
    -- 7FC0 - 7FCF : VGA_PALETTE
+   cdc_vga_memio_palette_inst : entity work.cdc
+   generic map (
+      G_WIDTH => 128
+   )
+   port map (
+      src_clk_i  => main_clk,
+      src_rst_i  => main_rst,
+      src_data_i => main_memio_wr(15*8+7 downto 0*8),
+      dst_clk_i  => vga_clk,
+      dst_data_o => vga_memio_palette
+   ); -- cdc_vga_memio_palette
+
    -- 7FD0 - 7FD1 : VGA_PIX_Y_INT
-   -- 7FD2 - 7FDE : Not used
-   -- 7FDF        : IRQ_MASK
-   vga_memio_palette   <= memio_wr(15*8+7 downto  0*8);
-   vga_memio_pix_y_int <= memio_wr(17*8+7 downto 16*8);
-   --                     memio_wr(30*8+7 downto 18*8);      -- Not used
-   irq_memio_mask      <= memio_wr(31*8+7 downto 31*8);
+   cdc_vga_memio_pix_y_int_inst : entity work.cdc
+   generic map (
+      G_WIDTH => 16
+   )
+   port map (
+      src_clk_i  => main_clk,
+      src_rst_i  => main_rst,
+      src_data_i => main_memio_wr(17*8+7 downto 16*8),
+      dst_clk_i  => vga_clk,
+      dst_data_o => vga_memio_pix_y_int
+   ); -- cdc_vga_memio_pix_y_int_inst
+
+   -- 7FD2 - 7FDF : Not used
+
 
    -- 7FE0 - 7FE1 : VGA_PIX_X
+   cdc_vga_memio_pix_x_inst : entity work.cdc
+   generic map (
+      G_WIDTH => 16
+   )
+   port map (
+      src_clk_i  => vga_clk,
+      src_rst_i  => vga_rst,
+      src_data_i => vga_memio_pix_x,
+      dst_clk_i  => main_clk,
+      dst_data_o => main_memio_rd(1*8+7 downto 0*8)
+   ); -- cdc_vga_memio_pix_x
+
    -- 7FE2 - 7FE3 : VGA_PIX_Y
-   -- 7FE4        : KBD_DATA
-   -- 7FE9 - 7FFE : Not used
-   -- 7FFF        : IRQ_STATUS
-   memio_rd( 1*8+7 downto  0*8) <= vga_memio_pix_x;
-   memio_rd( 3*8+7 downto  2*8) <= vga_memio_pix_y;
-   memio_rd( 4*8+7 downto  4*8) <= kbd_memio_data;
-   memio_rd(30*8+7 downto  5*8) <= (others => '0');   -- Not used
-   memio_rd(31*8+7 downto 31*8) <= irq_memio_status;
-   irq_memio_clear <= memio_rden(31);
+   cdc_vga_memio_pix_y : entity work.cdc
+   generic map (
+      G_WIDTH => 16
+   )
+   port map (
+      src_clk_i  => vga_clk,
+      src_rst_i  => vga_rst,
+      src_data_i => vga_memio_pix_y,
+      dst_clk_i  => main_clk,
+      dst_data_o => main_memio_rd(3*8+7 downto 2*8)
+   ); -- cdc_vga_memio_pix_y
 
-
-   -------------------------
-   -- Interrupt Sources
-   -------------------------
-
-   ic_irq(0) <= timer_irq;
-   ic_irq(1) <= vga_irq;
-   ic_irq(2) <= kbd_irq;
-   ic_irq(7 downto 3) <= (others => '0');             -- Not used
-
-
-   -------------------------
-   -- VGA overlay
-   -------------------------
-
-   vga_overlay(175 downto   0) <= cpu_debug;
-   vga_overlay(191 downto 176) <= kbd_debug;
+   -- 7FE4 - 7FFF : Not used
+   main_memio_rd(31*8+7 downto  4*8) <= (others => '0');   -- Not used
 
 end architecture structural;
 
