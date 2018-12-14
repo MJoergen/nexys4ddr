f39d847d456119367e60eef318f4e1c8df76aa11
Michael Jørgensen <mfj@silicom.dk>
2018-12-14 15:46:54 +0100

    Now uses unimacro instead of xpm, because the latter is in verilog and cannot be simulated using ghdl.

--- dyoc/Episodes/ep28_-_Tx_DMA/fpga/cdc.vhd
+++ dyoc/Episodes/ep28_-_Tx_DMA/fpga/cdc.vhd
@@ -2,8 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std_unsigned.all;
 
-Library xpm;
-use xpm.vcomponents.all;
+library unimacro;
 
 -- This module handles clock crossing using a Xilinx FIFO.
 -- The FIFO is written to whenever it is empty,
@@ -23,6 +22,11 @@ end cdc;
 
 architecture structural of cdc is
 
+   constant C_NUM_FIFOS : integer := (G_WIDTH+35) / 36;
+
+   signal src_data : std_logic_vector(C_NUM_FIFOS*36-1 downto 0);
+   signal dst_data : std_logic_vector(C_NUM_FIFOS*36-1 downto 0);
+
    signal rst             : std_logic := '1';
    signal rst_shr         : std_logic_vector(7 downto 0) := X"FF";
 
@@ -34,6 +38,9 @@ architecture structural of cdc is
    signal src_wr_rst_busy : std_logic;
 
 begin
+
+   src_data(G_WIDTH-1 downto 0) <= src_data_i;
+   dst_data_o <= dst_data(G_WIDTH-1 downto 0);
 
    rst_proc : process (src_clk_i)
    begin
@@ -50,48 +57,39 @@ begin
    -- Read when not empty
    dst_rd_en <= not dst_empty;
 
-   ---------------------------------------------
-   -- Instantiate FIFO to handle clock crossing
-   ---------------------------------------------
 
-   xpm_fifo_async_inst : xpm_fifo_async
-   generic map (
-      FIFO_READ_LATENCY    => 0,
-      FIFO_WRITE_DEPTH     => 16,      -- The smallest possible value
-      RD_DATA_COUNT_WIDTH  => 1,       -- Not used
-      READ_DATA_WIDTH      => G_WIDTH,
-      READ_MODE            => "fwft",
-      WRITE_DATA_WIDTH     => G_WIDTH,
-      WR_DATA_COUNT_WIDTH  => 1        -- Not used
-   )
-   port map (
-      almost_empty         => open,
-      almost_full          => open,
-      data_valid           => open,
-      dbiterr              => open ,
-      din                  => src_data_i,
-      dout                 => dst_data_o,
-      empty                => dst_empty,
-      full                 => open,
-      injectdbiterr        => '0',
-      injectsbiterr        => '0',
-      overflow             => open,
-      prog_empty           => open,
-      prog_full            => src_prog_full,
-      rd_clk               => dst_clk_i,
-      rd_data_count        => open,
-      rd_en                => dst_rd_en,
-      rd_rst_busy          => open,
-      rst                  => rst,
-      sbiterr              => open,
-      sleep                => '0',
-      underflow            => open,
-      wr_ack               => open,
-      wr_clk               => src_clk_i,
-      wr_data_count        => open,
-      wr_en                => src_wr_en,
-      wr_rst_busy          => src_wr_rst_busy
-   ); -- xpm_fifo_async_inst
+   ---------------------------------
+   -- Instantiate asynchronous FIFO
+   ---------------------------------
+
+   gen_fifos : for i in 0 to C_NUM_FIFOS-1 generate
+      i_fifo_dualclock_macro : entity unimacro.fifo_dualclock_macro
+         generic map (
+            DEVICE                  => "7SERIES",  -- Target Device: "VIRTEX5", "VIRTEX6", "7SERIES"
+            ALMOST_FULL_OFFSET      => X"0005",    -- Sets almost full threshold
+            ALMOST_EMPTY_OFFSET     => X"0080",    -- Sets the almost empty threshold
+            DATA_WIDTH              => 36,         -- Valid values are 1-72 (37-72 only valid when FIFO_SIZE="36Kb")
+            FIFO_SIZE               => "18Kb",     -- Target BRAM, "18Kb" or "36Kb"
+            FIRST_WORD_FALL_THROUGH => true
+         )
+         port map (
+            ALMOSTEMPTY => open,             -- 1-bit output almost empty
+            ALMOSTFULL  => src_prog_full,    -- 1-bit output almost full
+            DO          => dst_data(36*i+35 downto 36*i),       -- Output data, width defined by DATA_WIDTH parameter
+            EMPTY       => dst_empty,        -- 1-bit output empty
+            FULL        => open,             -- 1-bit output full
+            RDCOUNT     => open,             -- Output read count, width determined by FIFO depth
+            RDERR       => open,             -- 1-bit output read error
+            WRCOUNT     => open,             -- Output write count, width determined by FIFO depth
+            WRERR       => open,             -- 1-bit output write error
+            DI          => src_data(36*i+35 downto 36*i),       -- Input data, width defined by DATA_WIDTH parameter
+            RDCLK       => dst_clk_i,        -- 1-bit input read clock
+            RDEN        => dst_rd_en,        -- 1-bit input read enable
+            RST         => rst,              -- 1-bit input reset
+            WRCLK       => src_clk_i,        -- 1-bit input write clock
+            WREN        => src_wr_en         -- 1-bit input write enable
+         ); -- i_fifo_dualclock_macro
+   end generate gen_fifos;
 
 end architecture structural;
 
