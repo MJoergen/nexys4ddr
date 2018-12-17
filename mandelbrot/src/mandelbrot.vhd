a1d7ad1dd0a6e9a213d5b161bd436a1bf11531dc
Michael Jørgensen <mfj@silicom.dk>
2018-12-17 16:19:30 +0100

    Checkin time.

--- mandelbrot/src/mandelbrot.vhd
+++ mandelbrot/src/mandelbrot.vhd
@@ -8,12 +8,21 @@ use unisim.vcomponents.all;
 library unimacro;
 use unimacro.vcomponents.all;
 
+---------------------------
+-- This module performs the following calculations:
+-- new_x = (x+y)*(x-y) + cx
+-- new_y = (2x)*y + cy
+--
+-- It does it by using a single multiplier in a pipeline fashion
+-- Cycle 1 : Input to multiplier is (2x) and y.
+-- Cycle 2 : Input to multiplier is (x+y) and (x-y).
+
 entity mandelbrot is
    port (
       clk_i : in std_logic;
       rst_i : in std_logic;
-      x_i   : in std_logic_vector(17 downto 0);
-      y_i   : in std_logic_vector(17 downto 0);
+      cx_i  : in std_logic_vector(17 downto 0);
+      cy_i  : in std_logic_vector(17 downto 0);
       cnt_o : out std_logic_vector(9 downto 0)
    );
 end entity mandelbrot;
@@ -21,6 +30,8 @@ end entity mandelbrot;
 architecture rtl of mandelbrot is
 
    signal p : std_logic_vector(35 downto 0);
+   signal x : std_logic_vector(17 downto 0);
+   signal y : std_logic_vector(17 downto 0);
 
 begin
 
@@ -32,12 +43,12 @@ begin
       WIDTH_B => 18
    )
    port map (
+      CLK => clk_i,
+      RST => rst_i,
+      CE  => '1',
       P   => p,     -- Output
       A   => x_i,   -- Input
-      B   => y_i,   -- Input
-      CE  => '1',
-      CLK => clk_i,
-      RST => rst_i
+      B   => y_i    -- Input
    ); -- i_mult
 
    cnt_o <= p(9 downto 0);
