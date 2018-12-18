3324d75fc6d1667c0136bd6613c072257dfb7742
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2018-12-18 07:21:48 +0100

    Checkin time.

--- mandelbrot/sim/mandelbrot_tb.vhd
+++ mandelbrot/sim/mandelbrot_tb.vhd
@@ -2,6 +2,16 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std_unsigned.all;
 
+-- Simple testbench for the Mandelbrot state machine
+-- We start with the point -1+0.5i, i.e. cx = -1 and cy = 0.5
+-- The expected sequence of points is then:
+-- cnt |   x   |   y   |   x   |   y   
+-- ----+-------+-------+-------+--------
+--  0  | 00000 | 00000 |  0    |   0
+--  1  | 30000 | 08000 | -1    |   0.5
+--  2  | 3C000 | 38000 | -0.25 |  -0.5
+--  3  | 2D000 | 0C000 | -1.19 |   0.75
+
 entity mandelbrot_tb is
 end entity mandelbrot_tb;
 
@@ -12,10 +22,13 @@ architecture simulation of mandelbrot_tb is
    signal rst_d1 : std_logic := '1';
    signal rst_d2 : std_logic := '1';
 
-   signal cx   : std_logic_vector(17 downto 0);
-   signal cy   : std_logic_vector(17 downto 0);
-   signal cnt  : std_logic_vector( 9 downto 0);
-   signal done : std_logic;
+   signal start : std_logic;
+   signal cx    : std_logic_vector(17 downto 0);
+   signal cy    : std_logic_vector(17 downto 0);
+   signal x     : std_logic_vector(17 downto 0);
+   signal y     : std_logic_vector(17 downto 0);
+   signal cnt   : std_logic_vector( 9 downto 0);
+   signal done  : std_logic;
 
 begin
 
@@ -39,8 +52,20 @@ begin
    end process p_rst;
 
 
-   cx <= "01" & X"FFFF";
-   cy <= "00" & X"0000";
+   cx <= "11" & X"0000";   -- i.e. -1
+   cy <= "00" & X"8000";   -- i.e. 0.5
+
+   p_start : process
+   begin
+      start <= '0';
+      wait for 500 ns;
+      wait until clk = '1';
+      start <= '1';
+      wait until clk = '1';
+      start <= '0';
+      wait;
+   end process p_start;
+
 
    -------------------
    -- Instantiate DUT
@@ -48,12 +73,15 @@ begin
 
    i_mandelbrot : entity work.mandelbrot
       port map (
-         clk_i  => clk,
-         rst_i  => rst,
-         cx_i   => cx,
-         cy_i   => cy,
-         cnt_o  => cnt,
-         done_o => done
+         clk_i   => clk,
+         rst_i   => rst,
+         start_i => start,
+         cx_i    => cx,
+         cy_i    => cy,
+         x_o     => x,
+         y_o     => y,
+         cnt_o   => cnt,
+         done_o  => done
       ); -- i_mandelbrot
 
 end architecture simulation;
--- mandelbrot/src/mandelbrot.vhd
+++ mandelbrot/src/mandelbrot.vhd
@@ -2,29 +2,47 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std_unsigned.all;
 
---library unisim;
---use unisim.vcomponents.all;
---
---library unimacro;
---use unimacro.vcomponents.all;
+library unisim;
+use unisim.vcomponents.all;
+
+library unimacro;
+use unimacro.vcomponents.all;
 
 ---------------------------
--- This module performs the following calculations:
--- new_x = (x+y)*(x-y) + cx
--- new_y = (2x)*y + cy
+-- This module iterates the Mandelbrot fractal equation
+--    new_z = z^2 + c.
+-- Separating real and imaginary parts this becomes the following
+-- set of equations:
+--    new_x = (x+y)*(x-y) + cx
+--    new_y = (2x)*y + cy
+-- Inputs to this block are: cx_i and cy_i as well as start_i.
+-- start_i should be pulsed for one clock cycle.
+-- cx_i and cy_i must remain constant throughout the calculation.
 --
 -- It does it by using a single multiplier in a pipeline fashion
 -- Cycle 1 : Input to multiplier is (2x) and y.
 -- Cycle 2 : Input to multiplier is (x+y) and (x-y).
+--
+-- Real numbers are represented in 2.16 fixed point two's complement
+-- form, in the range -2 to 1.9. Examples
+-- -2   : 20000
+-- -1   : 30000
+-- -0.5 : 38000
+-- 0.5  : 08000
+-- 1    : 10000
+
 
 entity mandelbrot is
    port (
-      clk_i  : in  std_logic;
-      rst_i  : in  std_logic;
-      cx_i   : in  std_logic_vector(17 downto 0);
-      cy_i   : in  std_logic_vector(17 downto 0);
-      cnt_o  : out std_logic_vector( 9 downto 0);
-      done_o : out std_logic
+      clk_i   : in  std_logic;
+      rst_i   : in  std_logic;
+      start_i : in  std_logic;
+      cx_i    : in  std_logic_vector(17 downto 0);
+      cy_i    : in  std_logic_vector(17 downto 0);
+      x_o     : out std_logic_vector(17 downto 0);
+      y_o     : out std_logic_vector(17 downto 0);
+      cnt_o   : out std_logic_vector( 9 downto 0);
+      done_o  : out std_logic
    );
 end entity mandelbrot;
 
@@ -32,7 +50,6 @@ architecture rtl of mandelbrot is
 
    signal x         : std_logic_vector(17 downto 0);
    signal y         : std_logic_vector(17 downto 0);
-   signal state     : std_logic_vector( 1 downto 0);
    signal sum       : std_logic_vector(17 downto 0);
    signal diff      : std_logic_vector(17 downto 0);
    signal a         : std_logic_vector(17 downto 0);
@@ -42,15 +59,47 @@ architecture rtl of mandelbrot is
    signal cnt       : std_logic_vector( 9 downto 0);
    signal done      : std_logic;
 
+   type state_t is (IDLE_ST, ADD_ST, MULT_ST, UPDATE_X_ST, UPDATE_Y_ST);
+   signal state : state_t := IDLE_ST;
+
 begin
 
    p_state : process (clk_i)
    begin
       if rising_edge(clk_i) then
-         state <= state + 1;
+
+         done <= '0';
+
+         case state is
+            when IDLE_ST =>
+               if start_i = '1' then
+                  cnt   <= to_std_logic_vector(0, 10);
+                  state <= ADD_ST;
+               end if;
+
+            when ADD_ST =>
+               state <= MULT_ST;
+
+            when MULT_ST =>
+               state <= UPDATE_X_ST;
+
+            when UPDATE_X_ST =>
+               state <= UPDATE_Y_ST;
+               
+            when UPDATE_Y_ST =>
+               if x(17) = '1' or y(17) = '1' then
+                  done  <= '1';
+                  state <= IDLE_ST;
+               else
+                  cnt   <= cnt + 1;
+                  state <= ADD_ST;
+               end if;
+
+            when others => null;
+         end case;
 
          if rst_i = '1' then
-            state <= "00";
+            state <= IDLE_ST;
          end if;
       end if;
    end process p_state;
@@ -65,34 +114,27 @@ begin
    end process p_sum_diff;
 
 
-   a <= (x(16 downto 0) & '0') when state = 0 else
+   a <= (x(16 downto 0) & '0') when state = ADD_ST else
         sum;
 
-   b <= y when state = 0 else
+   b <= y when state = ADD_ST else
         diff;
 
---   i_mult : mult_macro
---   generic map (
---      DEVICE  => "7SERIES",
---      LATENCY => 1,
---      WIDTH_A => 18,
---      WIDTH_B => 18
---   )
---   port map (
---      CLK => clk_i,
---      RST => rst_i,
---      CE  => '1',
---      P   => product, -- Output
---      A   => a,       -- Input
---      B   => b        -- Input
---   ); -- i_mult
-
-   process (clk_i)
-   begin
-      if rising_edge(clk_i) then
-         product <= a*b;
-      end if;
-   end process;
+   i_mult : mult_macro
+   generic map (
+      DEVICE  => "7SERIES",
+      LATENCY => 1,
+      WIDTH_A => 18,
+      WIDTH_B => 18
+   )
+   port map (
+      CLK => clk_i,
+      RST => rst_i,
+      CE  => '1',
+      P   => product, -- Output
+      A   => a,       -- Input
+      B   => b        -- Input
+   ); -- i_mult
 
    p_product_d : process (clk_i)
    begin
@@ -104,46 +146,20 @@ begin
    p_xy : process (clk_i)
    begin
       if rising_edge(clk_i) then
-         if state = 2 then
+         if state = UPDATE_Y_ST then
             y <= product_d(35 downto 18) + cy_i;
          end if;
 
-         if state = 3 then
+         if state = UPDATE_X_ST then
             x <= product(35 downto 18) + cx_i;
          end if;
 
-         if rst_i = '1' then
+         if rst_i = '1' or start_i = '1' then
             x <= to_std_logic_vector(0, 18);
             y <= to_std_logic_vector(0, 18);
          end if;
       end if;
    end process p_xy;
-
-   p_cnt : process (clk_i)
-   begin
-      if rising_edge(clk_i) then
-         if state = 0 then
-            cnt <= cnt + 1;
-         end if;
-
-         if rst_i = '1' then
-            cnt <= to_std_logic_vector(0, 10);
-         end if;
-      end if;
-   end process p_cnt;
-
-   p_done : process (clk_i)
-   begin
-      if rising_edge(clk_i) then
-         if x(17) = '1' or y(17) = '1' then
-            done <= '1';
-         end if;
-
-         if rst_i = '1' then
-            done <= '0';
-         end if;
-      end if;
-   end process p_done;
 
 
    --------------------------
@@ -152,6 +168,8 @@ begin
 
    cnt_o  <= cnt;
    done_o <= done;
+   x_o    <= x;
+   y_o    <= y;
 
 end architecture rtl;
 
