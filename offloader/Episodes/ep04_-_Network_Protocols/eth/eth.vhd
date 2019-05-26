e8a607c9d1b41dc3482ed053c60559bad80bd56e
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2019-05-26 09:40:07 +0200

    Changed interface to byte2wide and wide2byte

--- offloader/Episodes/ep04_-_Network_Protocols/eth/eth.vhd
+++ offloader/Episodes/ep04_-_Network_Protocols/eth/eth.vhd
@@ -28,35 +28,34 @@ end eth;
 
 architecture Structural of eth is
 
-   signal rst          : std_logic                     := '1';
-   signal rst_cnt      : std_logic_vector(20 downto 0) := (others => '1');
-   signal debug        : std_logic_vector(255 downto 0);
+   signal rst      : std_logic                     := '1';
+   signal rst_cnt  : std_logic_vector(20 downto 0) := (others => '1');
+   signal debug    : std_logic_vector(255 downto 0);
+
+   -- Connected to eth_rx
+   signal rx_valid : std_logic;
+   signal rx_data  : std_logic_vector(7 downto 0);
+   signal rx_last  : std_logic;
+   signal rx_ok    : std_logic;
+
+   -- Output from strip_crc
+   signal st_valid : std_logic;
+   signal st_data  : std_logic_vector(7 downto 0);
+   signal st_last  : std_logic;
+
+   -- Output from byte2wide
+   signal pa_valid : std_logic;
+   signal pa_data  : std_logic_vector(42*8-1 downto 0);
+   signal pa_last  : std_logic;
+   signal pa_bytes : std_logic_vector(5 downto 0);
+   signal pa_first : std_logic;
 
    -- Connected to eth_tx
    -- TBD: For now, we just assign default values to these signals
-   signal tx_data      : std_logic_vector(7 downto 0)  := X"00";
-   signal tx_sof       : std_logic                     := '0';
-   signal tx_eof       : std_logic                     := '0';
-   signal tx_empty     : std_logic                     := '1';
-   signal tx_rden      : std_logic;
-
-   -- Connected to eth_rx
-   signal rx_data      : std_logic_vector(7 downto 0);
-   signal rx_sof       : std_logic;
-   signal rx_eof       : std_logic;
-   signal rx_valid     : std_logic;
-   signal rx_ok        : std_logic;
-
-   -- Output from strip_crc
-   signal st_data      : std_logic_vector(7 downto 0);
-   signal st_sof       : std_logic;
-   signal st_eof       : std_logic;
-   signal st_valid     : std_logic;
-
-   -- Output from byte2wide
-   signal pa_valid     : std_logic;
-   signal pa_data      : std_logic_vector(42*8-1 downto 0);
-   signal pa_size      : std_logic_vector(7 downto 0);
+   signal tx_data  : std_logic_vector(7 downto 0)  := X"00";
+   signal tx_last  : std_logic                     := '0';
+   signal tx_empty : std_logic                     := '1';
+   signal tx_rden  : std_logic;
 
 begin
 
@@ -68,11 +67,15 @@ begin
    p_debug : process (clk_i)
    begin
       if rising_edge(clk_i) then
-         if pa_valid = '1' then
+         if pa_valid = '1' and pa_first = '1' then
             debug <= pa_data(255 downto 0);
          end if;
+         if pa_valid = '1' then
+            pa_first <= pa_last;
+         end if;
          if rst = '1' then
-            debug <= (others => '1');  -- All ones means no frame received yet.
+            pa_first <= '1';
+            debug    <= (others => '1');  -- All ones means no frame received yet.
          end if;         
       end if;
    end process p_debug;
@@ -109,51 +112,44 @@ begin
 
    i_eth_rx : entity work.eth_rx
    port map (
-      eth_clk_i    => clk_i,
-      eth_rst_i    => rst,
-      rx_data_o    => rx_data,
-      rx_sof_o     => rx_sof,
-      rx_eof_o     => rx_eof,
-      rx_valid_o   => rx_valid,
-      rx_ok_o      => rx_ok,
-      eth_rxd_i    => eth_rxd_i,
-      eth_rxerr_i  => eth_rxerr_i,
-      eth_crsdv_i  => eth_crsdv_i
+      eth_clk_i   => clk_i,
+      eth_rst_i   => rst,
+      rx_valid_o  => rx_valid,
+      rx_data_o   => rx_data,
+      rx_last_o   => rx_last,
+      rx_ok_o     => rx_ok,
+      eth_rxd_i   => eth_rxd_i,
+      eth_rxerr_i => eth_rxerr_i,
+      eth_crsdv_i => eth_crsdv_i
    ); -- i_eth_rx
 
    i_strip_crc : entity work.strip_crc
    port map (
-      clk_i          => clk_i,
-      rst_i          => rst,
-      rx_valid_i     => rx_valid,
-      rx_sof_i       => rx_sof,
-      rx_eof_i       => rx_eof,
-      rx_ok_i        => rx_ok,
-      rx_data_i      => rx_data,
-      out_valid_o    => st_valid,
-      out_sof_o      => st_sof,
-      out_eof_o      => st_eof,
-      out_data_o     => st_data
+      clk_i       => clk_i,
+      rst_i       => rst,
+      rx_valid_i  => rx_valid,
+      rx_data_i   => rx_data,
+      rx_last_i   => rx_last,
+      rx_ok_i     => rx_ok,
+      out_valid_o => st_valid,
+      out_data_o  => st_data,
+      out_last_o  => st_last
    ); -- i_strip_crc
 
    i_byte2wide : entity work.byte2wide
    generic map (
-      G_HDR_SIZE  => 42          -- Size of ARP frame
+      G_SIZE     => 42          -- Size of ARP frame
    )
    port map (
-      clk_i       => clk_i,
-      rst_i       => rst,
-      rx_valid_i  => st_valid,
-      rx_sof_i    => st_sof,
-      rx_eof_i    => st_eof,
-      rx_data_i   => st_data,
-      hdr_valid_o => pa_valid,
-      hdr_data_o  => pa_data,
-      hdr_size_o  => pa_size,
-      hdr_more_o  => open,       -- Not used
-      pl_valid_o  => open,       -- Not used
-      pl_eof_o    => open,       -- Not used
-      pl_data_o   => open        -- Not used
+      clk_i      => clk_i,
+      rst_i      => rst,
+      rx_valid_i => st_valid,
+      rx_data_i  => st_data,
+      rx_last_i  => st_last,
+      tx_valid_o => pa_valid,
+      tx_data_o  => pa_data,
+      tx_last_o  => pa_last,
+      tx_bytes_o => pa_bytes
    ); -- i_wide2byte
 
 
@@ -166,8 +162,7 @@ begin
       eth_clk_i    => clk_i,
       eth_rst_i    => rst,
       tx_data_i    => tx_data,
-      tx_sof_i     => tx_sof,
-      tx_eof_i     => tx_eof,
+      tx_last_i    => tx_last,
       tx_empty_i   => tx_empty,
       tx_rden_o    => tx_rden,
       tx_err_o     => open,
--- offloader/Episodes/ep04_-_Network_Protocols/eth/tb_eth.vhd
+++ offloader/Episodes/ep04_-_Network_Protocols/eth/tb_eth.vhd
@@ -17,29 +17,61 @@ end tb_eth;
 
 architecture simulation of tb_eth is
 
-   signal clk          : std_logic;
-   signal rst          : std_logic;
+   type t_sim is record
+      valid : std_logic;
+      data  : std_logic_vector(64*8-1 downto 0);
+      size  : std_logic_vector(5 downto 0);
+   end record t_sim;
+
+   signal clk       : std_logic;
+   signal rst       : std_logic;
 
    -- Signals to control the generation of the Ethernet frames for transmission.
-   signal sim_tx_valid : std_logic;
-   signal sim_tx_size  : std_logic_vector(7 downto 0);
-   signal sim_tx_data  : std_logic_vector(64*8-1 downto 0);
+   signal sim_tx    : t_sim;
 
-   -- Input to eth_tx
-   signal tx_empty     : std_logic;
-   signal tx_rden      : std_logic;
-   signal tx_data      : std_logic_vector(7 downto 0);
-   signal tx_sof       : std_logic;
-   signal tx_eof       : std_logic;
+   -- Output from wide2byte
+   signal tx_empty  : std_logic;
+   signal tx_rden   : std_logic;
+   signal tx_data   : std_logic_vector(7 downto 0);
+   signal tx_last   : std_logic;
 
    -- Signals conected to DUT
-   signal eth_rstn     : std_logic;
-   signal eth_rxd      : std_logic_vector(1 downto 0);
-   signal eth_crsdv    : std_logic;
-   signal debug        : std_logic_vector(255 downto 0);
+   signal eth_rstn  : std_logic;
+   signal eth_rxd   : std_logic_vector(1 downto 0);
+   signal eth_crsdv : std_logic;
+   signal debug     : std_logic_vector(255 downto 0);
 
    -- Signal to control execution of the testbench.
    signal test_running : std_logic := '1';
+
+   -- Common procedure to test a given frame size.
+   procedure test_frame(constant size : in    integer;
+                        signal   tx   : inout t_sim;
+                        signal   rx   : in    std_logic_vector(255 downto 0)) is
+   begin
+      report "Testing frame size " & integer'image(size);
+
+      -- Send one frame
+      wait until clk = '0';
+      tx.data <= (others => 'U');
+      for i in 0 to size-1 loop
+         tx.data(64*8-1-8*i downto 64*8-8-8*i) <= to_std_logic_vector(i+12, 8);
+      end loop;
+      tx.size  <= to_stdlogicvector(size mod 64, 6);
+      tx.valid <= '1';
+      wait until clk = '1';
+      tx.valid <= '0';
+
+      wait for size*20 ns;
+
+      -- Validate received frame
+      if size > 42 then
+         assert debug(32*8-1 downto 42*8-42*8) = tx.data(54*8-1 downto 64*8-42*8);
+      else
+         assert debug(32*8-1 downto 42*8-size*8) = tx.data(54*8-1 downto 64*8-size*8);
+      end if;
+
+   end procedure test_frame;
 
 begin
 
@@ -69,19 +101,19 @@ begin
 
    i_wide2byte : entity work.wide2byte
    generic map (
-      G_PL_SIZE  => 64
+      G_SIZE     => 64
    )
    port map (
       clk_i      => clk,
       rst_i      => rst,
-      pl_valid_i => sim_tx_valid,
-      pl_size_i  => sim_tx_size,
-      pl_data_i  => sim_tx_data,
+      rx_valid_i => sim_tx.valid,
+      rx_data_i  => sim_tx.data,
+      rx_last_i  => '1',
+      rx_bytes_i => sim_tx.size,
       --
       tx_empty_o => tx_empty,
       tx_data_o  => tx_data,
-      tx_sof_o   => tx_sof,
-      tx_eof_o   => tx_eof,
+      tx_last_o  => tx_last,
       tx_rden_i  => tx_rden
    ); -- i_wide2byte
 
@@ -89,10 +121,9 @@ begin
    port map (
       eth_clk_i  => clk,
       eth_rst_i  => rst,
-      tx_data_i  => tx_data,
-      tx_sof_i   => tx_sof,
-      tx_eof_i   => tx_eof,
       tx_empty_i => tx_empty,
+      tx_data_i  => tx_data,
+      tx_last_i  => tx_last,
       tx_rden_o  => tx_rden,
       tx_err_o   => open,
       --
@@ -129,42 +160,20 @@ begin
    main_test_proc : process
    begin
       -- Wait until reset is complete
-      sim_tx_valid <= '0';
+      sim_tx.valid <= '0';
       wait until rst = '0';
       wait until eth_rstn = '1';
       wait until clk = '1';
 
-      -- Send one frame (16 bytes)
-      sim_tx_data <= (others => 'U');
-      for i in 0 to 15 loop
-         sim_tx_data(64*8-1-8*i downto 64*8-8-8*i) <= to_std_logic_vector(i+12, 8);
-      end loop;
-      sim_tx_size  <= X"10";
-      sim_tx_valid <= '1';
-      wait until clk = '1';
-      sim_tx_valid <= '0';
+      test_frame(16, sim_tx, debug);
 
-      -- Make a short pause while frame is being received.
-      wait for 400 ns;
+      wait for 100 ns; -- Make a short pause for easier debugging.
 
-      -- Validate received frame
-      assert debug(32*8-1 downto 32*8-16*8) = sim_tx_data(64*8-1 downto 64*8-16*8);
+      test_frame(32, sim_tx, debug);
 
-      -- Send another frame (32 bytes)
-      sim_tx_data <= (others => 'U');
-      for i in 0 to 31 loop
-         sim_tx_data(64*8-1-8*i downto 64*8-8-8*i) <= to_std_logic_vector(i+22, 8);
-      end loop;
-      sim_tx_size  <= X"20";
-      sim_tx_valid <= '1';
-      wait until clk = '1';
-      sim_tx_valid <= '0';
+      wait for 100 ns; -- Make a short pause for easier debugging.
 
-      -- Make a short pause while frame is being received.
-      wait for 500 ns;
-
-      -- Validate received frame
-      assert debug(32*8-1 downto 0) = sim_tx_data(64*8-1 downto 64*8-32*8);
+      test_frame(60, sim_tx, debug);
 
       -- Stop test
       wait until clk = '1';
