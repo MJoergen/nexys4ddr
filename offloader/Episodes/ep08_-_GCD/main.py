d0d92a87d50a627a7cad5cc54a588224565cc6ab
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2019-05-31 16:12:59 +0200

    Added test of MULT.

--- offloader/Episodes/ep08_-_GCD/main.py
+++ offloader/Episodes/ep08_-_GCD/main.py
@@ -14,17 +14,17 @@ sock = socket.socket(socket.AF_INET,    # Internet
 # IP address and port number of the FPGA device
 DUT = ("192.168.1.77", 4660)
 
-def offloader_gcd(num1, num2):
+def offloader(cmd, num1, num2):
     # Convert the numbers to a hex string. Remove the preceding "0x" and the trailing "L".
     str1 = hex(num1)[2:-1]
     str2 = hex(num2)[2:-1]
 
     # Prepend string with 0's.
-    str1 = "0"*(18-len(str1)) + str1
-    str2 = "0"*(18-len(str2)) + str2
+    str1 = "0"*(16-len(str1)) + str1
+    str2 = "0"*(16-len(str2)) + str2
 
     # Prepare message to send to offloader
-    message = str1.decode("hex") + str2.decode("hex")
+    message = cmd.decode("hex") + str1.decode("hex") + str2.decode("hex")
 
     #print "Sending message: ",message.encode('hex')
     sock.sendto(message, DUT)
@@ -40,6 +40,12 @@ num1 = 12345678901234567890
 num2 =  9876543210987654321
 
 print "The two numbers are:", num1, "and", num2
-print "GCD calculated by offloader:", offloader_gcd(num1, num2)
+print "GCD calculated by offloader:", offloader("0101", num1, num2)
 print "GCD calculated by python:   ", python_gcd(num1, num2)
+
+num1 = 123456789
+num2 = 987654321
+
+print "MULT calculated by offloader:", offloader("0102", num1, num2)
+print "MULT calculated by python:   ", num1*num2
 
