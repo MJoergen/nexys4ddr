8a352af88ba41f0f6f3bae8f97946d4252634887
Michael Jørgensen <mfj@silicom.dk>
2019-06-04 15:44:36 +0200

    Cleanup.

--- offloader/Episodes/ep10_-_Fact/alg/alg.vhd
+++ offloader/Episodes/ep10_-_Fact/alg/alg.vhd
@@ -35,32 +35,10 @@ end alg;
 
 architecture Structural of alg is
 
-   constant C_ZERO    : std_logic_vector(G_SIZE-1 downto 0) := (others => '0');
-
-   constant C_PRIMES1 : std_logic_vector(63 downto 0) := X"088886ffdb344692"; -- 2*3*5*7*11*13*17*19*23*29*31*37*41*43*47
-   constant C_PRIMES2 : std_logic_vector(63 downto 0) := X"34091fa96ffdf47b"; -- 53*59*61*67*71*73*79*83*89*97
-   constant C_PRIMES3 : std_logic_vector(63 downto 0) := X"3c47d8d728a77ebb"; -- 101*103*107*109*113*127*131*137*139
-   constant C_PRIMES4 : std_logic_vector(63 downto 0) := X"077ab7da9d709ea9"; -- 149*151*157*163*167*173*179*181
-
-   signal cf_start    : std_logic;
    signal cf_res_x    : std_logic_vector(2*G_SIZE-1 downto 0);
    signal cf_res_y    : std_logic_vector(G_SIZE-1 downto 0);
    signal cf_res_neg  : std_logic;
    signal cf_valid    : std_logic;
-
-   type res2_vector is array (natural range <>) of std_logic_vector(2*G_SIZE-1 downto 0);
-   type res_vector is array (natural range <>) of std_logic_vector(G_SIZE-1 downto 0);
-
-   signal fact_primes : std_logic_vector(G_SIZE-1 downto 0);
-   signal fact_x      : res2_vector(G_NUM_FACTS-1 downto 0);
-   signal fact_val    : res_vector(G_NUM_FACTS-1 downto 0);
-   signal fact_neg    : std_logic_vector(G_NUM_FACTS-1 downto 0);
-   signal fact_start  : std_logic_vector(G_NUM_FACTS-1 downto 0);
-   signal fact_res    : res_vector(G_NUM_FACTS-1 downto 0);
-   signal fact_busy   : std_logic_vector(G_NUM_FACTS-1 downto 0);
-   signal fact_valid  : std_logic_vector(G_NUM_FACTS-1 downto 0);
-
-   signal fact_idx    : integer range 0 to G_NUM_FACTS-1;
 
 begin
 
