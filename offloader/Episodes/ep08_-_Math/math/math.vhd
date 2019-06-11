f2662386e357bf18009ddbcebfb45c56269ffe99
Michael Jørgensen <mfj@silicom.dk>
2019-06-11 11:00:58 +0200

    Cleanup.

--- offloader/Episodes/ep08_-_Math/math/math.vhd
+++ offloader/Episodes/ep08_-_Math/math/math.vhd
@@ -30,126 +30,57 @@ end math;
 
 architecture Structural of math is
 
-   constant C_ZERO      : std_logic_vector(G_SIZE-1 downto 0) := (others => '0');
-   constant C_ZERO_HALF : std_logic_vector(G_SIZE/2-1 downto 0) := (others => '0');
-
-   signal cmd           : std_logic_vector(15 downto 0);
-
-   signal val1          : std_logic_vector(G_SIZE-1 downto 0);
-   signal val2          : std_logic_vector(G_SIZE-1 downto 0);
-   signal val3          : std_logic_vector(G_SIZE-1 downto 0);
-   signal val4          : std_logic_vector(G_SIZE-1 downto 0);
-
-   signal mult_start    : std_logic;
-   signal mult_res      : std_logic_vector(G_SIZE-1 downto 0);
-   signal mult_valid    : std_logic;
-
-   signal gcd_start     : std_logic;
-   signal gcd_res       : std_logic_vector(G_SIZE-1 downto 0);
-   signal gcd_valid     : std_logic;
-
-   signal divmod_start  : std_logic;
-   signal divmod_res_q  : std_logic_vector(G_SIZE-1 downto 0);
-   signal divmod_res_r  : std_logic_vector(G_SIZE-1 downto 0);
-   signal divmod_valid  : std_logic;
-
-   signal amm_start     : std_logic;
-   signal amm_res       : std_logic_vector(G_SIZE-1 downto 0);
-   signal amm_valid     : std_logic;
-
-   signal res           : std_logic_vector(2*G_SIZE-1 downto 0);
-   signal valid         : std_logic;
-
    signal debug         : std_logic_vector(255 downto 0);
+
+   signal sqrt_val      : std_logic_vector(2*G_SIZE-1 downto 0);  -- N
+   signal sqrt_start    : std_logic;
+   signal sqrt_res      : std_logic_vector(G_SIZE-1 downto 0);    -- M = floor(sqrt(N))
+   signal sqrt_diff     : std_logic_vector(G_SIZE-1 downto 0);    -- N - M*M
+   signal sqrt_busy     : std_logic;
+   signal sqrt_valid    : std_logic;
 
 begin
 
    -- We just ignore rx_last_i and rx_bytes_i.
-   cmd   <= rx_data_i(60*8-1          downto 58*8);
-   val1  <= rx_data_i(58*8-1          downto 58*8-G_SIZE);
-   val2  <= rx_data_i(58*8-1-G_SIZE   downto 58*8-G_SIZE*2);
-   val3  <= rx_data_i(58*8-1-2*G_SIZE downto 58*8-G_SIZE*3);
-   val4  <= rx_data_i(58*8-1-3*G_SIZE downto 58*8-G_SIZE*4);
+   sqrt_val   <= rx_data_i(60*8-1 downto 60*8-2*G_SIZE);
+   sqrt_start <= rx_valid_i;
 
-   mult_start   <= rx_valid_i when cmd = X"0101" else '0';
-   gcd_start    <= rx_valid_i when cmd = X"0102" else '0';
-   divmod_start <= rx_valid_i when cmd = X"0103" else '0';
-   amm_start    <= rx_valid_i when cmd = X"0104" else '0';
+   ---------------------------
+   -- Instantiate SQRT module
+   ---------------------------
 
-   i_mult : entity work.mult
+   i_sqrt : entity work.sqrt
    generic map (
       G_SIZE => G_SIZE
    )
    port map ( 
       clk_i   => clk_i,
       rst_i   => rst_i,
-      val1_i  => val1,
-      val2_i  => val2,
-      start_i => mult_start,
-      res_o   => mult_res,
-      valid_o => mult_valid
-   ); -- i_mult
+      val_i   => sqrt_val,
+      start_i => sqrt_start,
+      res_o   => sqrt_res,
+      diff_o  => sqrt_diff,
+      busy_o  => sqrt_busy,
+      valid_o => sqrt_valid
+   ); -- i_sqrt
 
-   i_gcd : entity work.gcd
-   generic map (
-      G_SIZE => G_SIZE
-   )
-   port map ( 
-      clk_i   => clk_i,
-      rst_i   => rst_i,
-      val1_i  => val1,
-      val2_i  => val2,
-      start_i => gcd_start,
-      res_o   => gcd_res,
-      valid_o => gcd_valid
-   ); -- i_gcd
 
-   i_divmod : entity work.divmod
-   generic map (
-      G_SIZE => G_SIZE
-   )
-   port map ( 
-      clk_i   => clk_i,
-      rst_i   => rst_i,
-      val_n_i => val1,
-      val_d_i => val2,
-      start_i => divmod_start,
-      res_q_o => divmod_res_q,
-      res_r_o => divmod_res_r,
-      valid_o => divmod_valid
-   ); -- i_divmod
+   ------------------------
+   -- Drive output signals
+   ------------------------
 
-   i_amm : entity work.amm
-   generic map (
-      G_SIZE => G_SIZE/2
-   )
-   port map ( 
-      clk_i   => clk_i,
-      rst_i   => rst_i,
-      val_a_i => val1(G_SIZE/2-1 downto 0),
-      val_x_i => val2,
-      val_b_i => val3,
-      val_n_i => val4,
-      start_i => amm_start,
-      res_o   => amm_res,
-      valid_o => amm_valid
-   ); -- i_amm
+   p_out : process (clk_i)
+   begin
+      if rising_edge(clk_i) then
+         tx_valid_o <= sqrt_valid;
+         tx_data_o(60*8-1          downto 60*8-G_SIZE)   <= sqrt_res;
+         tx_data_o(60*8-1-G_SIZE   downto 60*8-2*G_SIZE) <= sqrt_diff;
+         tx_data_o(60*8-1-2*G_SIZE downto 0)             <= (others => '0');
+         tx_bytes_o <= to_stdlogicvector(2*G_SIZE/8, 6);
+         tx_last_o  <= '1';
+      end if;
+   end process p_out;
 
-   valid <= mult_valid   or
-            gcd_valid    or
-            divmod_valid or
-            amm_valid;
-
-   res   <= mult_res     & C_ZERO                                   or
-            gcd_res      & C_ZERO                                   or
-            divmod_res_q & divmod_res_r                             or
-            amm_res      & C_ZERO;
-
-   tx_valid_o <= valid;
-   tx_data_o(60*8-1        downto 60*8-2*G_SIZE) <= res;
-   tx_data_o(60*8-1-2*G_SIZE downto 0)           <= (others => '0');
-   tx_bytes_o <= to_stdlogicvector(18, 6);
-   tx_last_o  <= '1';
 
    p_debug : process (clk_i)
    begin
@@ -157,9 +88,9 @@ begin
          if rx_valid_i = '1' then
             debug <= rx_data_i(60*8-1 downto 60*8-256);
          end if;
-         if valid = '1' then
+         if sqrt_valid = '1' then
             debug <= (others => '0');
-            debug(2*G_SIZE-1 downto 0) <= res;
+            debug(2*G_SIZE-1 downto 0) <= sqrt_res & sqrt_diff;
          end if;
       end if;
    end process p_debug;
--- offloader/Episodes/ep08_-_Math/math/tb_math.vhd
+++ offloader/Episodes/ep08_-_Math/math/tb_math.vhd
@@ -83,33 +83,31 @@ begin
 
    main_test_proc : process
 
-      -- Verify MULT processing
-      procedure verify_mult(val1 : integer;
-                            val2 : integer;
-                            res  : integer) is
+      -- Verify SQRT processing
+      procedure verify_sqrt(val  : integer;
+                            res  : integer;
+                            diff : integer) is
       begin
 
-         report "Verify MULT: " & integer'image(val1) & 
-         "*" & integer'image(val2) & "=" & integer'image(res);
+         report "Verify SQRT: " & integer'image(val) & 
+         " -> " & integer'image(res) & ", " & integer'image(diff);
 
          cmd.valid <= '1';
          cmd.data  <= (others => '0');
-         cmd.data(60*8-1 downto 42*8)  <= X"0101" &
-            to_stdlogicvector(val1, C_SIZE) & 
-            to_stdlogicvector(val2, C_SIZE) &
-            C_ZERO &
-            C_ZERO;
+         cmd.data(60*8-1 downto 60*8-2*C_SIZE) <=
+            to_stdlogicvector(val, 2*C_SIZE);
          cmd.last  <= '1';
-         cmd.bytes <= to_stdlogicvector(18, 6);
+         cmd.bytes <= to_stdlogicvector(2*C_SIZE/8, 6);
          wait until clk = '1';
          cmd.valid <= '0';
 
          -- Build expected response
          exp.data  <= (others => '0');
-         exp.data(60*8-1 downto 60*8-C_SIZE)  <= 
-            to_stdlogicvector(res,C_SIZE);
+         exp.data(60*8-1 downto 60*8-2*C_SIZE) <= 
+            to_stdlogicvector(res, C_SIZE) &
+            to_stdlogicvector(diff, C_SIZE);
          exp.last  <= '1';
-         exp.bytes <= to_stdlogicvector(18, 6);
+         exp.bytes <= to_stdlogicvector(2*C_SIZE/8, 6);
 
          -- Verify received response is correct
          wait until clk = '1' and resp.valid = '1';
@@ -119,188 +117,16 @@ begin
          assert resp.bytes = exp.bytes;
          wait until clk = '1' and resp.valid = '0';
          wait until clk = '0';
-      end procedure verify_mult;
-
-      -- Verify GCD processing
-      procedure verify_gcd(val1 : integer;
-                           val2 : integer;
-                           res  : integer) is
-      begin
-
-         report "Verify GCD: " & integer'image(val1) & 
-         "*" & integer'image(val2) & "=" & integer'image(res);
-
-         cmd.valid <= '1';
-         cmd.data  <= (others => '0');
-         cmd.data(60*8-1 downto 42*8)  <= X"0102" &
-            to_stdlogicvector(val1, C_SIZE) & 
-            to_stdlogicvector(val2, C_SIZE) &
-            C_ZERO &
-            C_ZERO;
-         cmd.last  <= '1';
-         cmd.bytes <= to_stdlogicvector(18, 6);
-         wait until clk = '1';
-         cmd.valid <= '0';
-
-         -- Build expected response
-         exp.data  <= (others => '0');
-         exp.data(60*8-1 downto 60*8-C_SIZE)  <= 
-            to_stdlogicvector(res,C_SIZE);
-         exp.last  <= '1';
-         exp.bytes <= to_stdlogicvector(18, 6);
-
-         -- Verify received response is correct
-         wait until clk = '1' and resp.valid = '1';
-         wait until clk = '0';
-         assert resp.data  = exp.data;
-         assert resp.last  = exp.last;
-         assert resp.bytes = exp.bytes;
-         wait until clk = '1' and resp.valid = '0';
-         wait until clk = '0';
-      end procedure verify_gcd;
-
-      -- Verify DIVMOD processing
-      procedure verify_divmod(val_n : integer;
-                              val_d : integer;
-                              res_q : integer;
-                              res_r : integer) is
-      begin
-
-         report "Verify DIVMOD: " & integer'image(val_n) & 
-         "/" & integer'image(val_d) & "=" & integer'image(res_q) & ", " & integer'image(res_r);
-
-         cmd.valid <= '1';
-         cmd.data  <= (others => '0');
-         cmd.data(60*8-1 downto 42*8)  <= X"0103" &
-            to_stdlogicvector(val_n, C_SIZE) & 
-            to_stdlogicvector(val_d, C_SIZE) &
-            C_ZERO &
-            C_ZERO;
-         cmd.last  <= '1';
-         cmd.bytes <= to_stdlogicvector(18, 6);
-         wait until clk = '1';
-         cmd.valid <= '0';
-
-         -- Build expected response
-         exp.data  <= (others => '0');
-         exp.data(60*8-1 downto 60*8-2*C_SIZE)  <= 
-            to_stdlogicvector(res_q, C_SIZE) &
-            to_stdlogicvector(res_r, C_SIZE);
-         exp.last  <= '1';
-         exp.bytes <= to_stdlogicvector(18, 6);
-
-         -- Verify received response is correct
-         wait until clk = '1' and resp.valid = '1';
-         wait until clk = '0';
-         assert resp.data  = exp.data;
-         assert resp.last  = exp.last;
-         assert resp.bytes = exp.bytes;
-         wait until clk = '1' and resp.valid = '0';
-         wait until clk = '0';
-      end procedure verify_divmod;
-
-      -- Verify AMM processing
-      procedure verify_amm(val_a : integer;
-                           val_x : integer;
-                           val_b : integer;
-                           val_n : integer;
-                           res   : integer) is
-      begin
-
-         report "Verify AMM: " & integer'image(val_a) & 
-         "*" & integer'image(val_x) & "+" & integer'image(val_b) & " mod " & integer'image(val_n) &
-         "=" & integer'image(res);
-
-         cmd.valid <= '1';
-         cmd.data  <= (others => '0');
-         cmd.data(60*8-1 downto 42*8)  <= X"0104" &
-            to_stdlogicvector(val_a, C_SIZE) & 
-            to_stdlogicvector(val_x, C_SIZE) &
-            to_stdlogicvector(val_b, C_SIZE) &
-            to_stdlogicvector(val_n, C_SIZE);
-         cmd.last  <= '1';
-         cmd.bytes <= to_stdlogicvector(18, 6);
-         wait until clk = '1';
-         cmd.valid <= '0';
-
-         -- Build expected response
-         exp.data  <= (others => '0');
-         exp.data(60*8-1 downto 60*8-C_SIZE)  <= 
-            to_stdlogicvector(res, C_SIZE);
-         exp.last  <= '1';
-         exp.bytes <= to_stdlogicvector(18, 6);
-
-         -- Verify received response is correct
-         wait until clk = '1' and resp.valid = '1';
-         wait until clk = '0';
-         assert resp.data  = exp.data;
-         assert resp.last  = exp.last;
-         assert resp.bytes = exp.bytes;
-         wait until clk = '1' and resp.valid = '0';
-         wait until clk = '0';
-      end procedure verify_amm;
+      end procedure verify_sqrt;
 
    begin
       -- Wait until reset is complete
       cmd.valid <= '0';
       wait until clk = '1' and rst = '0';
 
-      -- Verify MULT
-      for a in 0 to 3 loop 
-         for b in 0 to 3 loop 
-            verify_mult(a, b, a*b);
-         end loop;
-      end loop;
+      -- Verify SQRT
+      verify_sqrt(  7,  2, 3);
 
-      verify_mult(  7, 13,   91);
-      verify_mult(100, 10, 1000);
-
-      -- Verify GCD
-      verify_gcd(0, 0, 0);
-      verify_gcd(1, 0, 0);
-      verify_gcd(0, 1, 0);
-      verify_gcd(1, 1, 1);
-      verify_gcd(1, 2, 1);
-      verify_gcd(1, 3, 1);
-      verify_gcd(1, 4, 1);
-      verify_gcd(2, 1, 1);
-      verify_gcd(2, 2, 2);
-      verify_gcd(2, 3, 1);
-      verify_gcd(2, 4, 2);
-      verify_gcd(3, 1, 1);
-      verify_gcd(3, 2, 1);
-      verify_gcd(3, 3, 3);
-      verify_gcd(3, 4, 1);
-      verify_gcd(4, 1, 1);
-      verify_gcd(4, 2, 2);
-      verify_gcd(4, 3, 1);
-      verify_gcd(4, 4, 4);
-      verify_gcd(30, 35, 5);
-      verify_gcd(35, 30, 5);
-      verify_gcd(36, 30, 6);
-      verify_gcd(36, 32, 4);
-      verify_gcd(37, 30, 1);
-      verify_gcd(70, 30, 10);
-      verify_gcd(150, 30, 30);
-      verify_gcd(250, 30, 10);
-      verify_gcd(253, 30, 1);
-      verify_gcd(252, 30, 6);
-      
-      -- Verify DIVMOD
-      for n in 0 to 20 loop 
-         verify_divmod(n, 3, n/3, n mod 3);
-      end loop;
-
-      for d in 1 to 20 loop 
-         verify_divmod(10, d, 10/d, 10 mod d);
-      end loop;
-
-      verify_divmod(131*251+14, 131, 251, 14);
-      verify_divmod(131*251+14, 251, 131, 14);
-
-      -- Verify AMM
-      verify_amm(7, 13, 21, 30, 22); -- 7*13+21 mod 30 = 22
- 
       -- Stop test
       wait until clk = '1';
       report "Test completed";
