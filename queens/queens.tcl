967954e037bb6a87ce00aeff931f28547469c188
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2021-01-01 17:42:04 +0100

    queens: Add queens.tcl, needed by new Makefile

---
+++ queens/queens.tcl
@@ -0,0 +1,26 @@
+# This is a tcl command script for the Vivado tool chain
+#read_vhdl -vhdl2008 { \
+
+read_vhdl { \
+   src/bitmap_pkg.vhd \
+   src/clk.vhd \
+   src/clk_wiz_0.vhd \
+   src/clk_wiz_0_clk_wiz.vhd \
+   src/counter.vhd \
+   src/digit.vhd \
+   src/disp_queens.vhd \
+   src/int2seg.vhd \
+   src/queens.vhd \
+   src/queens_top.vhd \
+   src/seg.vhd \
+   src/vga_ctrl.vhd
+}
+read_xdc src/queens_top.xdc
+synth_design -top queens_top -part xc7a100tcsg324-1 -flatten_hierarchy none
+place_design
+route_design
+write_checkpoint -force queens.dcp
+write_bitstream -force queens.bit
+exit
+
+
