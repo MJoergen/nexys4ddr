7c75cec8c10fe5f11838efdee708c23d260f9fe2
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2021-01-01 20:07:56 +0100

    queens: Add simulation

--- queens/src/queens_tb.vhd
+++ queens/src/queens_tb.vhd
@@ -1,116 +1,107 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    21:01:12 01/11/2013 
--- Design Name: 
--- Module Name:    dragon - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity queens_tb is
-end queens_tb;
+end entity queens_tb;
 
-architecture Behavioral of queens_tb is
+architecture simulation of queens_tb is
 
-    constant NUM_QUEENS : integer := 4;
+   constant G_NUM_QUEENS : integer := 4;
 
-    subtype queens_t is std_logic_vector(NUM_QUEENS-1 downto 0);
-    subtype board_t is std_logic_vector(NUM_QUEENS*NUM_QUEENS-1 downto 0);
+   subtype queens_t is std_logic_vector(G_NUM_QUEENS-1 downto 0);
+   subtype board_t is std_logic_vector(G_NUM_QUEENS*G_NUM_QUEENS-1 downto 0);
 
-    signal clk    : std_logic;
-    signal rst    : std_logic;
-    signal enable : std_logic;
-    signal valid  : std_logic;
-    signal done   : std_logic;
-    signal count  : integer range 0 to 100000;
+   signal clk     : std_logic;
+   signal rst     : std_logic;
+   signal enable  : std_logic;
+   signal running : std_logic := '1';
 
-    signal board  : board_t;
+   signal board   : board_t;
+   signal valid   : std_logic;
+   signal done    : std_logic;
 
-    subtype row_t is std_logic_vector(NUM_QUEENS-1 downto 0);
-    type row_vector is array(natural range <>) of row_t;
-    signal board_rows : row_vector(NUM_QUEENS-1 downto 0);
+   signal count   : integer range 0 to 100000;
+
+   subtype row_t is std_logic_vector(G_NUM_QUEENS-1 downto 0);
+   type row_vector is array(natural range <>) of row_t;
+   signal board_rows : row_vector(G_NUM_QUEENS-1 downto 0);
 
 begin
 
-    gen_rows: for row in 0 to NUM_QUEENS-1 generate
-        board_rows(row) <= board(row*NUM_QUEENS + NUM_QUEENS-1 downto row*NUM_QUEENS);
-    end generate;
- 
-    inst_queens : entity work.queens
-        generic map (
-            NUM_QUEENS => NUM_QUEENS
-            )
-        port map (
-			clk_i   => clk,
-			rst_i   => rst,
-            en_i    => enable,
-			board_o => board,
-            valid_o => valid,
-            done_o  => done
-            );
-  
-    rst_driver : rst <= '1', '0' after 40 ns;
+   p_clk : process
+   begin
+      clk <= '1', '0' after 10 ns;
+      wait for 20 ns;
+      if running = '0' then
+         wait;
+      end if;
+   end process p_clk;
 
-	clk_driver : process
-	begin
-        clk <= '1', '0' after 10 ns;
-        wait for 20 ns;
-    end process clk_driver;
+   rst <= '1', '0' after 40 ns;
 
-    enable_driver : process(clk, rst)
-    begin
-        if rst = '1' then
+   p_enable : process (clk)
+   begin
+      if rising_edge(clk) then
+         enable <= not enable;
+
+         if rst = '1' then
             enable <= '0';
-        elsif rising_edge(clk) then
-            enable <= not enable;
-        end if;
-    end process enable_driver;
+         end if;
+      end if;
+   end process p_enable;
 
-    valid_count : process (clk, rst)
-        variable rows_or  : std_logic_vector(NUM_QUEENS-1 downto 0);
-        constant ROW_ONES : std_logic_vector(NUM_QUEENS-1 downto 0) := (others => '1');
-    begin
-        if rst = '1' then
-            count <= 0;
-        elsif rising_edge(clk) then
-            if valid = '1' and enable = '1' then
-                count <= count + 1;
-            end if;
 
-            rows_or := (others => '0');
-            for row in 0 to NUM_QUEENS-1 loop
-                rows_or := rows_or or board_rows(row);
-            end loop;
+   -------------------
+   -- Instantiate DUT
+   -------------------
 
+   i_queens : entity work.queens
+      generic map (
+         G_NUM_QUEENS => G_NUM_QUEENS
+      )
+      port map (
+         clk_i   => clk,
+         rst_i   => rst,
+         en_i    => enable,
+         board_o => board,
+         valid_o => valid,
+         done_o  => done
+      ); -- i_queens
+
+
+   gen_rows: for row in 0 to G_NUM_QUEENS-1 generate
+      board_rows(row) <= board(row*G_NUM_QUEENS + G_NUM_QUEENS-1 downto row*G_NUM_QUEENS);
+   end generate;
+
+   p_count : process (clk)
+      variable rows_or  : std_logic_vector(G_NUM_QUEENS-1 downto 0);
+      constant ROW_ONES : std_logic_vector(G_NUM_QUEENS-1 downto 0) := (others => '1');
+   begin
+      if rising_edge(clk) then
+         if valid = '1' and enable = '1' then
+            count <= count + 1;
+         end if;
+
+         rows_or := (others => '0');
+         for row in 0 to G_NUM_QUEENS-1 loop
+            rows_or := rows_or or board_rows(row);
+         end loop;
+
+         if rst /= '1' then
             assert (valid = '0' or (rows_or = ROW_ONES));
             assert (done = '0' or (count = 2));
-            if done = '1' then
-                assert false report "End of simulation.";
-            end if;
-        end if;
-    end process valid_count;
+         end if;
 
-end Behavioral;
+         if done = '1' then
+            report "End of simulation.";
+            running <= '0';
+         end if;
+
+         if rst = '1' then
+            count <= 0;
+         end if;
+      end if;
+   end process p_count;
+
+end architecture simulation;
 
--- queens/src/queens_top_tb.vhd
+++ queens/src/queens_top_tb.vhd
@@ -1,117 +1,111 @@
-----------------------------------------------------------------------------------
--- Company:  Granbo
--- Engineer: Michael Jørgensen
--- 
--- The file contains the top level test bench for the timer_demo
-----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+--use ieee.std_logic_unsigned.all;
 
 entity queens_top_tb is
-end queens_top_tb;
+end entity queens_top_tb;
 
-architecture Structural of queens_top_tb is
+architecture simulation of queens_top_tb is
 
-    constant NUM_QUEENS : integer := 4;
-    constant FREQ       : integer := 5;
+   constant NUM_QUEENS : integer := 4;
+   constant FREQ       : integer := 5;
 
     -- Clock
-    signal clk   : std_logic;  -- 100 MHz
+   signal clk   : std_logic;  -- 100 MHz
 
     -- LED, buttom, and switches
-    signal led       : std_logic_vector (7 downto 0); 
-    signal sw        : std_logic_vector (7 downto 0); 
+   signal led       : std_logic_vector (7 downto 0);
+   signal sw        : std_logic_vector (7 downto 0);
 
     -- VGA port
-    signal vga_hs    : std_logic; 
-    signal vga_vs    : std_logic;
-    signal vga_red   : std_logic_vector (3 downto 0); 
-    signal vga_green : std_logic_vector (3 downto 0); 
-    signal vga_blue  : std_logic_vector (3 downto 0); 
+   signal vga_hs    : std_logic;
+   signal vga_vs    : std_logic;
+   signal vga_red   : std_logic_vector (3 downto 0);
+   signal vga_green : std_logic_vector (3 downto 0);
+   signal vga_blue  : std_logic_vector (3 downto 0);
 
     -- Output segment display
-    signal seg_ca    : std_logic_vector (6 downto 0);
-    signal seg_dp    : std_logic;
-    signal seg_an    : std_logic_vector (3 downto 0);
+   signal seg_ca    : std_logic_vector (6 downto 0);
+   signal seg_dp    : std_logic;
+   signal seg_an    : std_logic_vector (3 downto 0);
 
-    signal board     : std_logic_vector (NUM_QUEENS*NUM_QUEENS-1 downto 0);
-    signal num_solutions : std_logic_vector(13 downto 0);
-    signal valid    : std_logic;
-    signal done     : std_logic;
-    signal enable   : std_logic;
+   signal board     : std_logic_vector (NUM_QUEENS*NUM_QUEENS-1 downto 0);
+   signal num_solutions : std_logic_vector(13 downto 0);
+   signal valid    : std_logic;
+   signal done     : std_logic;
+   signal enable   : std_logic;
 
-    signal count     : integer;
- 
-    signal test_running : boolean := true;
+   signal count     : integer;
+
+   signal test_running : boolean := true;
 
 begin
 
     -- Generate clock and reset
-    clk_gen : process
-    begin
+   clk_gen : process
+   begin
       if not test_running then
-        wait;
+         wait;
       end if;
 
       clk <= '1', '0' after 5 ns; -- 100 MHz
       wait for 10 ns;
-    end process clk_gen;
+   end process clk_gen;
 
-    rst_gen : process
-    begin
+   rst_gen : process
+   begin
       sw(0) <= '1', '0' after 100 ns;
       wait;
-    end process rst_gen;
+   end process rst_gen;
 
-    sw(7 downto 1) <= "0000101"; -- Stop the stepping
+   sw(7 downto 1) <= "0000101"; -- Stop the stepping
 
     -- Instantiate DUT
-    inst_queens_top : entity work.queens_top
-    generic map (
-        FREQ       => FREQ  ,
-        NUM_QUEENS => NUM_QUEENS
-        )
-    port map (
-        clk_i       => clk       ,
-        sw_i        => sw        ,
-        led_o       => led       ,
-        board_o         => board         ,
-        num_solutions_o => num_solutions ,
-        valid_o         => valid         ,
-        done_o          => done          ,
-        enable_o        => enable        ,
-        vga_hs_o    => vga_hs    ,
-        vga_vs_o    => vga_vs    ,
-        vga_red_o   => vga_red   ,
-        vga_green_o => vga_green ,
-        vga_blue_o  => vga_blue  
-        );
+   i_queens_top : entity work.queens_top
+      generic map (
+         G_FREQ       => FREQ,
+         G_NUM_QUEENS => NUM_QUEENS
+      )
+      port map (
+         clk_i       => clk,
+         sw_i        => sw,
+         led_o       => led,
+--         board_o         => board         ,
+--         num_solutions_o => num_solutions ,
+--         valid_o         => valid         ,
+--         done_o          => done          ,
+--         enable_o        => enable        ,
+         vga_hs_o    => vga_hs,
+         vga_vs_o    => vga_vs,
+         vga_red_o   => vga_red,
+         vga_green_o => vga_green,
+         vga_blue_o  => vga_blue
+      );
 
-    valid_count : process (clk, sw(0))
-        variable rows_or  : std_logic_vector(NUM_QUEENS-1 downto 0);
-        constant ROW_ONES : std_logic_vector(NUM_QUEENS-1 downto 0) := (others => '1');
-    begin
-        if sw(0) = '1' then
-            count <= 0;
-        elsif rising_edge(clk) then
-            if valid = '1' and enable = '1' then
-                count <= count + 1;
-            end if;
+   valid_count : process (clk, sw(0))
+      variable rows_or  : std_logic_vector(NUM_QUEENS-1 downto 0);
+      constant ROW_ONES : std_logic_vector(NUM_QUEENS-1 downto 0) := (others => '1');
+   begin
+      if sw(0) = '1' then
+         count <= 0;
+      elsif rising_edge(clk) then
+         if valid = '1' and enable = '1' then
+            count <= count + 1;
+         end if;
 
-            rows_or := (others => '0');
-            for row in 0 to NUM_QUEENS-1 loop
-                rows_or := rows_or or board(row*NUM_QUEENS + NUM_QUEENS-1 downto row*NUM_QUEENS);
-            end loop;
+         rows_or := (others => '0');
+         for row in 0 to NUM_QUEENS-1 loop
+            rows_or := rows_or or board(row*NUM_QUEENS + NUM_QUEENS-1 downto row*NUM_QUEENS);
+         end loop;
 
-            assert (valid = '0' or (rows_or = ROW_ONES));
-            assert (done = '0' or (count = 2));
-            if done = '1' then
-              report "End of simulation.";
-              test_running <= false;
-            end if;
-        end if;
-    end process valid_count;
-   
-end Structural;
+         assert (valid = '0' or (rows_or = ROW_ONES));
+         assert (done = '0' or (count = 2));
+         if done = '1' then
+            report "End of simulation.";
+            test_running <= false;
+         end if;
+      end if;
+   end process valid_count;
+
+end architecture simulation;
 
